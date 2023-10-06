65df6a8ca21cfc6668b07dc7569de6e7dc2a8ae4
Aleksandar Savchev <aleksandar.savchev@sap.com>
2023-10-06 10:29:10 +0300

    Improve 242436 rule

--- pkg/provider/gardener/ruleset/disak8sstig/v1r10/242436.go
+++ pkg/provider/gardener/ruleset/disak8sstig/v1r10/242436.go
@@ -11,6 +11,8 @@ import (
 	"slices"
 	"strings"
 
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	kubeutils "github.com/gardener/diki/pkg/kubernetes/utils"
@@ -36,26 +38,45 @@ func (r *Rule242436) Name() string {
 
 func (r *Rule242436) Run(ctx context.Context) (rule.RuleResult, error) {
 	const (
-		kapiName = "kube-apiserver"
-		option   = "enable-admission-plugins"
+		kapiName                = "kube-apiserver"
+		enableAdmissionPlugins  = "enable-admission-plugins"
+		disableAdmissionPlugins = "disable-admission-plugins"
 	)
 	target := gardener.NewTarget("cluster", "seed", "name", kapiName, "namespace", r.Namespace, "kind", "deployment")
 
-	optSlice, err := kubeutils.GetCommandOptionFromDeployment(ctx, r.Client, kapiName, kapiName, r.Namespace, option)
-	if err != nil {
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      kapiName,
+			Namespace: r.Namespace,
+		},
+	}
+
+	if err := r.Client.Get(ctx, client.ObjectKeyFromObject(deployment), deployment); err != nil {
 		return rule.SingleCheckResult(r, rule.ErroredCheckResult(err.Error(), target)), nil
 	}
 
-	// option defaults to allowed value ValidatingAdmissionWebhook
+	container, found := kubeutils.GetContainerFromDeployment(deployment, kapiName)
+	if !found {
+		return rule.SingleCheckResult(r, rule.ErroredCheckResult(fmt.Sprintf("deployment: %s does not contain container: %s", kapiName, kapiName), target)), nil
+	}
+
+	enableAdmissionPluginsSlice := kubeutils.FindFlagValueRaw(append(container.Command, container.Args...), enableAdmissionPlugins)
+	disableAdmissionPluginsSlice := kubeutils.FindFlagValueRaw(append(container.Command, container.Args...), disableAdmissionPlugins)
+
+	// enable-admission-plugins defaults to allowed value ValidatingAdmissionWebhook
 	// see https://kubernetes.io/docs/reference/command-line-tools-reference/kube-apiserver/
 	switch {
-	case len(optSlice) == 0:
-		return rule.SingleCheckResult(r, rule.PassedCheckResult(fmt.Sprintf("Option %s has not been set.", option), target)), nil
-	case len(optSlice) > 1:
-		return rule.SingleCheckResult(r, rule.WarningCheckResult(fmt.Sprintf("Option %s has been set more than once in container command.", option), target)), nil
-	case slices.Contains(strings.Split(optSlice[0], ","), "ValidatingAdmissionWebhook"):
-		return rule.SingleCheckResult(r, rule.PassedCheckResult(fmt.Sprintf("Option %s set to allowed value.", option), target)), nil
+	case len(enableAdmissionPluginsSlice) == 0:
+		return rule.SingleCheckResult(r, rule.PassedCheckResult(fmt.Sprintf("Option %s has not been set.", enableAdmissionPlugins), target)), nil
+	case len(enableAdmissionPluginsSlice) > 1:
+		return rule.SingleCheckResult(r, rule.WarningCheckResult(fmt.Sprintf("Option %s has been set more than once in container command.", enableAdmissionPlugins), target)), nil
+	case len(disableAdmissionPluginsSlice) > 1:
+		return rule.SingleCheckResult(r, rule.WarningCheckResult(fmt.Sprintf("Option %s has been set more than once in container command.", disableAdmissionPlugins), target)), nil
+	case len(disableAdmissionPluginsSlice) == 1 && slices.Contains(strings.Split(disableAdmissionPluginsSlice[0], ","), "ValidatingAdmissionWebhook"):
+		return rule.SingleCheckResult(r, rule.FailedCheckResult(fmt.Sprintf("Option %s set to not allowed value.", disableAdmissionPlugins), target)), nil
+	case slices.Contains(strings.Split(enableAdmissionPluginsSlice[0], ","), "ValidatingAdmissionWebhook"):
+		return rule.SingleCheckResult(r, rule.PassedCheckResult(fmt.Sprintf("Option %s set to allowed value.", enableAdmissionPlugins), target)), nil
 	default:
-		return rule.SingleCheckResult(r, rule.FailedCheckResult(fmt.Sprintf("Option %s set to not allowed value.", option), target)), nil
+		return rule.SingleCheckResult(r, rule.FailedCheckResult(fmt.Sprintf("Option %s set to not allowed value.", enableAdmissionPlugins), target)), nil
 	}
 }
