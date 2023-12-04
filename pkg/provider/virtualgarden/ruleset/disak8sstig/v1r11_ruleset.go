499a80a69ccff0d21773bb47017c856a3c833764
Dimitar Mirchev <dimitar.mirchev@sap.com>
2023-12-04 15:37:53 +0200

    Skip 242408

--- pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
+++ pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
@@ -221,6 +221,12 @@ func (r *Ruleset) registerV1R11Rules(ruleOptions map[string]config.RuleOptionsCo
 			noKubeletsMsg,
 			rule.Skipped,
 		),
+		rule.NewSkipRule(
+			sharedv1r11.ID242408,
+			"The Kubernetes manifest files must have least privileges (MEDIUM 242408)",
+			"Gardener does not deploy any control plane component as systemd processes or static pod.",
+			rule.Skipped,
+		),
 	}
 
 	for i, r := range rules {
