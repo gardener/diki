d02a26240fef0d50b051b6eac03d3db9eb65508a
Dimitar Mirchev <dimitar.mirchev@sap.com>
2023-12-04 15:48:33 +0200

    Skip 242412

--- pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
+++ pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
@@ -245,6 +245,12 @@ func (r *Ruleset) registerV1R11Rules(ruleOptions map[string]config.RuleOptionsCo
 			"The Virtual Garden cluster does not make use of a Kubernetes Scheduler.",
 			rule.Skipped,
 		),
+		rule.NewSkipRule(
+			sharedv1r11.ID242412,
+			"The Kubernetes Controllers must enforce ports, protocols, and services (PPS) that adhere to the Ports, Protocols, and Services Management Category Assurance List (PPSM CAL) (MEDIUM 242412)",
+			"Cannot be tested and should be enforced organizationally. Gardener uses a minimum of known and automatically opened/used/created ports/protocols/services (PPSM stands for Ports, Protocols, Service Management).",
+			rule.Skipped,
+		),
 	}
 
 	for i, r := range rules {
