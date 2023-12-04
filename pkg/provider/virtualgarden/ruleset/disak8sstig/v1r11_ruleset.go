266b00983cb78ca1fef07e6c58c0f28314a7c923
Dimitar Mirchev <dimitar.mirchev@sap.com>
2023-12-04 15:54:52 +0200

    Skip 242415

--- pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
+++ pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
@@ -264,6 +264,12 @@ func (r *Ruleset) registerV1R11Rules(ruleOptions map[string]config.RuleOptionsCo
 			noPodsMsg,
 			rule.Skipped,
 		),
+		rule.NewSkipRule(
+			sharedv1r11.ID242415,
+			"Secrets in Kubernetes must not be stored as environment variables (HIGH 242415)",
+			noPodsMsg,
+			rule.Skipped,
+		),
 	}
 
 	for i, r := range rules {
