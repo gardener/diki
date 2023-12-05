36e896e05361653a03d6c1039cdc4be78b959711
Dimitar Mirchev <dimitar.mirchev@sap.com>
2023-12-05 11:18:16 +0200

    Share 242464

--- pkg/shared/ruleset/disak8sstig/v1r11/242464.go
+++ pkg/shared/ruleset/disak8sstig/v1r11/242464.go
@@ -7,7 +7,6 @@ package v1r11
 import (
 	"context"
 	"fmt"
-	"log/slog"
 	"strconv"
 
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -19,9 +18,10 @@ import (
 var _ rule.Rule = &Rule242464{}
 
 type Rule242464 struct {
-	Client    client.Client
-	Namespace string
-	Logger    *slog.Logger
+	Client         client.Client
+	Namespace      string
+	DeploymentName string
+	ContainerName  string
 }
 
 func (r *Rule242464) ID() string {
@@ -29,17 +29,24 @@ func (r *Rule242464) ID() string {
 }
 
 func (r *Rule242464) Name() string {
-	return "Kubernetes API Server audit log retention must be set (MEDIUM 242464)"
+	return "The Kubernetes API Server audit log retention must be set (MEDIUM 242464)"
 }
 
 func (r *Rule242464) Run(ctx context.Context) (rule.RuleResult, error) {
-	const (
-		kapiName = "kube-apiserver"
-		option   = "audit-log-maxage"
-	)
-	target := rule.NewTarget("cluster", "seed", "name", kapiName, "namespace", r.Namespace, "kind", "deployment")
+	const option = "audit-log-maxage"
+	deploymentName := "kube-apiserver"
+	containerName := "kube-apiserver"
 
-	optSlice, err := kubeutils.GetCommandOptionFromDeployment(ctx, r.Client, kapiName, kapiName, r.Namespace, option)
+	if r.DeploymentName != "" {
+		deploymentName = r.DeploymentName
+	}
+
+	if r.ContainerName != "" {
+		containerName = r.ContainerName
+	}
+	target := rule.NewTarget("name", deploymentName, "namespace", r.Namespace, "kind", "deployment")
+
+	optSlice, err := kubeutils.GetCommandOptionFromDeployment(ctx, r.Client, deploymentName, containerName, r.Namespace, option)
 	if err != nil {
 		return rule.SingleCheckResult(r, rule.ErroredCheckResult(err.Error(), target)), nil
 	}
