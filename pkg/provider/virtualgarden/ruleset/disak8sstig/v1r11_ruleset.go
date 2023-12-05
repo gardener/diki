42d18eeff68914a7cbe691fefde02007432c29f7
Dimitar Mirchev <dimitar.mirchev@sap.com>
2023-12-05 10:21:25 +0200

    Skip 242437

--- pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
+++ pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
@@ -384,6 +384,12 @@ func (r *Ruleset) registerV1R11Rules(ruleOptions map[string]config.RuleOptionsCo
 			DeploymentName: apiserverDeploymentName,
 			ContainerName:  apiserverContainerName,
 		},
+		rule.NewSkipRule(
+			sharedv1r11.ID242437,
+			"Kubernetes must have a pod security policy set (HIGH 242437)",
+			"PSPs are removed in K8s version 1.25.",
+			rule.Skipped,
+		),
 	}
 
 	for i, r := range rules {
