72198466dffa9701fba057c2add3999dd641f0f1
Dimitar Mirchev <dimitar.mirchev@sap.com>
2023-12-05 10:52:52 +0200

    Skip 242450

--- pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
+++ pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
@@ -443,6 +443,12 @@ func (r *Ruleset) registerV1R11Rules(ruleOptions map[string]config.RuleOptionsCo
 			noKubeletsMsg,
 			rule.Skipped,
 		),
+		rule.NewSkipRule(
+			sharedv1r11.ID242450,
+			"The Kubernetes Kubelet certificate authority must be owned by root (MEDIUM 242450)",
+			noKubeletsMsg,
+			rule.Skipped,
+		),
 	}
 
 	for i, r := range rules {
