7b3388190950a70772cf4c305fd882921231ca0c
Dimitar Mirchev <dimitar.mirchev@sap.com>
2023-12-05 10:54:27 +0200

    Skip 242452

--- pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
+++ pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
@@ -455,6 +455,12 @@ func (r *Ruleset) registerV1R11Rules(ruleOptions map[string]config.RuleOptionsCo
 			"",
 			rule.NotImplemented,
 		),
+		rule.NewSkipRule(
+			sharedv1r11.ID242452,
+			"The Kubernetes kubelet KubeConfig must have file permissions set to 644 or more restrictive (MEDIUM 242452)",
+			noKubeletsMsg,
+			rule.Skipped,
+		),
 	}
 
 	for i, r := range rules {
