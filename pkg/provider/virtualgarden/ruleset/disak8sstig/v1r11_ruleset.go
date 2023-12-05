599a9f4d522415eea22b0f1b4f15ae97d92b8150
Dimitar Mirchev <dimitar.mirchev@sap.com>
2023-12-05 11:45:13 +0200

    Skip 254801

--- pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
+++ pkg/provider/virtualgarden/ruleset/disak8sstig/v1r11_ruleset.go
@@ -588,6 +588,12 @@ func (r *Ruleset) registerV1R11Rules(ruleOptions map[string]config.RuleOptionsCo
 			DeploymentName: apiserverDeploymentName,
 			ContainerName:  apiserverContainerName,
 		},
+		rule.NewSkipRule(
+			sharedv1r11.ID254801,
+			"Kubernetes must enable PodSecurity admission controller on static pods and Kubelets (HIGH 254801)",
+			noKubeletsMsg,
+			rule.Skipped,
+		),
 	}
 
 	for i, r := range rules {
