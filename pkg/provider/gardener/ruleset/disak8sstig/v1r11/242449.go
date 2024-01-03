f1429898a381c05268c5acd40991decd606dc503
Aleksandar Savchev <aleksandar.savchev@sap.com>
2024-01-03 14:32:00 +0200

    Implement rule 242449

--- pkg/provider/gardener/ruleset/disak8sstig/v1r11/242449.go
+++ pkg/provider/gardener/ruleset/disak8sstig/v1r11/242449.go
@@ -6,22 +6,102 @@ package v1r11
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
+	"strings"
 
+	"github.com/gardener/diki/imagevector"
+	"github.com/gardener/diki/pkg/kubernetes/pod"
+	kubeutils "github.com/gardener/diki/pkg/kubernetes/utils"
+	"github.com/gardener/diki/pkg/provider/gardener/internal/utils"
+	"github.com/gardener/diki/pkg/provider/gardener/ruleset"
 	"github.com/gardener/diki/pkg/rule"
 )
 
 var _ rule.Rule = &Rule242449{}
 
-type Rule242449 struct{}
+type Rule242449 struct {
+	InstanceID        string
+	ClusterPodContext pod.PodContext
+	Logger            *slog.Logger
+}
 
 func (r *Rule242449) ID() string {
 	return ID242449
 }
 
 func (r *Rule242449) Name() string {
-	return "Kubernetes Kubelet certificate authority file must have file permissions set to 644 or more restrictive (MEDIUM 242449)"
+	return "The Kubernetes Kubelet certificate authority file must have file permissions set to 644 or more restrictive (MEDIUM 242449)"
 }
 
-func (r *Rule242449) Run(_ context.Context) (rule.RuleResult, error) {
-	return rule.SingleCheckResult(r, rule.SkippedCheckResult(`Rule implemented by "node-files" for correctness, consistency, deduplication, reliability, and performance reasons.`, rule.NewTarget())), nil
+func (r *Rule242449) Run(ctx context.Context) (rule.RuleResult, error) {
+	var clientCAFilePath string
+	podName := fmt.Sprintf("diki-%s-%s", r.ID(), Generator.Generate(10))
+	execPodTarget := rule.NewTarget("cluster", "shoot", "name", podName, "namespace", "kube-system", "kind", "pod")
+	image, err := imagevector.ImageVector().FindImage(ruleset.OpsToolbeltImageName)
+	if err != nil {
+		return rule.RuleResult{}, fmt.Errorf("failed to find image version for %s: %w", ruleset.OpsToolbeltImageName, err)
+	}
+	defer func() {
+		if err := r.ClusterPodContext.Delete(ctx, podName, "kube-system"); err != nil {
+			r.Logger.Error(err.Error())
+		}
+	}()
+	additionalLabels := map[string]string{
+		pod.LabelInstanceID: r.InstanceID,
+	}
+	podExecutor, err := r.ClusterPodContext.Create(ctx, pod.NewPrivilegedPod(podName, "kube-system", image.String(), "", additionalLabels))
+	if err != nil {
+		return rule.SingleCheckResult(r, rule.ErroredCheckResult(err.Error(), execPodTarget)), nil
+	}
+
+	rawKubeletCommand, err := kubeutils.GetKubeletCommand(ctx, podExecutor)
+	if err != nil {
+		return rule.SingleCheckResult(r, rule.ErroredCheckResult(fmt.Sprintf("could not retrieve kubelet command: %s", err.Error()), execPodTarget)), nil
+	}
+
+	if len(rawKubeletCommand) > 0 {
+		if kubeletConfig, err := kubeutils.GetKubeletConfig(ctx, podExecutor, rawKubeletCommand); err != nil {
+			return rule.SingleCheckResult(r, rule.ErroredCheckResult(fmt.Sprintf("could not retrieve kubelet config: %s", err.Error()), execPodTarget)), nil
+		} else {
+			switch {
+			case kubeletConfig.Authentication.X509.ClientCAFile == nil:
+				return rule.SingleCheckResult(r, rule.FailedCheckResult("could not find client ca path: client-ca-file not set.", execPodTarget)), nil
+			case strings.TrimSpace(*kubeletConfig.Authentication.X509.ClientCAFile) == "":
+				return rule.SingleCheckResult(r, rule.FailedCheckResult("could not find client ca path: client-ca-file is empty.", execPodTarget)), nil
+			default:
+				clientCAFilePath = *kubeletConfig.Authentication.X509.ClientCAFile
+			}
+		}
+	} else {
+		return rule.SingleCheckResult(r, rule.ErroredCheckResult("could not retrieve kubelet config: kubelet command not retrived", execPodTarget)), nil
+	}
+
+	target := rule.NewTarget("cluster", "shoot", "details", fmt.Sprintf("filePath: %s", clientCAFilePath))
+	statsRaw, err := podExecutor.Execute(ctx, "/bin/sh", fmt.Sprintf(`stat -Lc "%%a %%u %%g %%n" %s`, clientCAFilePath))
+	if err != nil {
+		return rule.SingleCheckResult(r, rule.ErroredCheckResult(err.Error(), execPodTarget)), nil
+	}
+	if len(statsRaw) == 0 {
+		return rule.SingleCheckResult(r, rule.ErroredCheckResult("Stats not found", target)), nil
+	}
+	stat := strings.Split(strings.TrimSpace(statsRaw), "\n")[0]
+
+	statSlice := strings.Split(stat, " ")
+	expectedFilePermissionsMax := "644"
+	filePermissions := statSlice[0]
+	fileName := strings.Join(statSlice[3:], " ")
+
+	exceedFilePermissions, err := utils.ExceedFilePermissions(filePermissions, expectedFilePermissionsMax)
+	if err != nil {
+		return rule.SingleCheckResult(r, rule.ErroredCheckResult(err.Error(), target)), nil
+	}
+
+	if exceedFilePermissions {
+		detailedTarget := target.With("details", fmt.Sprintf("fileName: %s, permissions: %s, expectedPermissionsMax: %s", fileName, filePermissions, expectedFilePermissionsMax))
+		return rule.SingleCheckResult(r, rule.FailedCheckResult("File has too wide permissions", detailedTarget)), nil
+	}
+
+	detailedTarget := target.With("details", fmt.Sprintf("fileName: %s, permissions: %s", fileName, filePermissions))
+	return rule.SingleCheckResult(r, rule.PassedCheckResult("File has expected permissions", detailedTarget)), nil
 }
--- pkg/provider/gardener/ruleset/disak8sstig/v1r11/242449_test.go
+++ pkg/provider/gardener/ruleset/disak8sstig/v1r11/242449_test.go
@@ -10,28 +10,69 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	"github.com/gardener/diki/pkg/kubernetes/pod"
+	fakepod "github.com/gardener/diki/pkg/kubernetes/pod/fake"
 	"github.com/gardener/diki/pkg/provider/gardener/ruleset/disak8sstig/v1r11"
 	"github.com/gardener/diki/pkg/rule"
 )
 
 var _ = Describe("#242449", func() {
+	const (
+		rawKubeletCommand = `--config=/var/lib/kubelet/config/kubelet`
+		kubeletConfig     = `authentication:
+  x509:
+    clientCAFile: /var/lib/kubelet/ca.crt
+`
+		notSetClientCAFileConfig = `authentication:
+  webhook:
+    enabled: true
+`
+		compliantCAFileStats    = `644 0 0 /var/lib/kubelet/ca.crt`
+		nonCompliantCAFileStats = `700 0 0 /var/lib/kubelet/ca.crt`
+	)
 	var (
-		ctx = context.TODO()
+		instanceID            = "1"
+		fakeClusterPodContext pod.PodContext
+		dikiPodName           = "diki-242449-aaaaaaaaaa"
+		ctx                   = context.TODO()
 	)
 
-	It("should skip rules with correct message", func() {
-		r := &v1r11.Rule242449{}
-
-		ruleResult, err := r.Run(ctx)
-		Expect(err).ToNot(HaveOccurred())
-
-		Expect(ruleResult.CheckResults).To(Equal([]rule.CheckResult{
-			{
-				Status:  rule.Skipped,
-				Message: `Rule implemented by "node-files" for correctness, consistency, deduplication, reliability, and performance reasons.`,
-				Target:  rule.NewTarget(),
-			},
-		},
-		))
+	BeforeEach(func() {
+		v1r11.Generator = &FakeRandString{CurrentChar: 'a'}
 	})
+
+	DescribeTable("Run cases",
+		func(clusterExecuteReturnString [][]string, clusterExecuteReturnError [][]error, expectedCheckResults []rule.CheckResult) {
+			fakeClusterPodContext = fakepod.NewFakeSimplePodContext(clusterExecuteReturnString, clusterExecuteReturnError)
+			r := &v1r11.Rule242449{
+				Logger:            testLogger,
+				InstanceID:        instanceID,
+				ClusterPodContext: fakeClusterPodContext,
+			}
+
+			ruleResult, err := r.Run(ctx)
+			Expect(err).To(BeNil())
+
+			Expect(ruleResult.CheckResults).To(Equal(expectedCheckResults))
+		},
+
+		Entry("should return passed checkResult when file complies",
+			[][]string{{rawKubeletCommand, kubeletConfig, compliantCAFileStats}},
+			[][]error{{nil, nil, nil}},
+			[]rule.CheckResult{
+				rule.PassedCheckResult("File has expected permissions", rule.NewTarget("cluster", "shoot", "details", "fileName: /var/lib/kubelet/ca.crt, permissions: 644")),
+			}),
+		Entry("should return failed checkResults when file does not comply",
+			[][]string{{rawKubeletCommand, kubeletConfig, nonCompliantCAFileStats}},
+			[][]error{{nil, nil, nil}},
+			[]rule.CheckResult{
+				rule.FailedCheckResult("File has too wide permissions", rule.NewTarget("cluster", "shoot", "details", "fileName: /var/lib/kubelet/ca.crt, permissions: 700, expectedPermissionsMax: 644")),
+			}),
+		Entry("should return failed checkResults when clientCAFile is not set",
+			[][]string{{rawKubeletCommand, notSetClientCAFileConfig}},
+			[][]error{{nil, nil}},
+			[]rule.CheckResult{
+				rule.FailedCheckResult("could not find client ca path: client-ca-file not set.", rule.NewTarget("cluster", "shoot", "name", dikiPodName, "namespace", "kube-system", "kind", "pod")),
+			}),
+	)
 })
