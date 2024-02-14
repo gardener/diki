10d5c7e7baa53c99ff9dd6084787c887b2cefbe4
Aleksandar Savchev <aleksandar.savchev@sap.com>
2024-02-14 15:17:02 +0200

    Implement shared rule 242417

---
+++ pkg/shared/ruleset/disak8sstig/v1r11/242417.go
@@ -0,0 +1,110 @@
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1r11
+
+import (
+	"context"
+	"fmt"
+
+	"golang.org/x/exp/slices"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/selection"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/gardener/diki/pkg/internal/utils"
+	"github.com/gardener/diki/pkg/kubernetes/pod"
+	kubeutils "github.com/gardener/diki/pkg/kubernetes/utils"
+	"github.com/gardener/diki/pkg/rule"
+	"github.com/gardener/diki/pkg/shared/provider"
+)
+
+var _ rule.Rule = &Rule242417{}
+
+type Rule242417 struct {
+	Client  client.Client
+	Options *Options242417
+	Logger  provider.Logger
+}
+
+type Options242417 struct {
+	AcceptedPods []AcceptedPods242417 `json:"acceptedPods" yaml:"acceptedPods"`
+}
+
+type AcceptedPods242417 struct {
+	PodMatchLabels map[string]string `json:"podMatchLabels" yaml:"podMatchLabels"`
+	NamespaceNames []string          `json:"namespaceNames" yaml:"namespaceNames"`
+	Justification  string            `json:"justification" yaml:"justification"`
+	Status         string            `json:"status" yaml:"status"`
+}
+
+func (r *Rule242417) ID() string {
+	return ID242417
+}
+
+func (r *Rule242417) Name() string {
+	return "Kubernetes must separate user functionality (MEDIUM 242417)"
+}
+
+func (r *Rule242417) Run(ctx context.Context) (rule.RuleResult, error) {
+	checkResults := []rule.CheckResult{}
+	systemNamespaces := []string{"kube-system", "kube-public", "kube-node-lease"}
+	acceptedPods := []AcceptedPods242417{}
+
+	if r.Options != nil {
+		acceptedPods = r.Options.AcceptedPods
+	}
+
+	notDikiPodReq, err := labels.NewRequirement(pod.LabelComplianceRoleKey, selection.NotEquals, []string{pod.LabelComplianceRolePrivPod})
+	if err != nil {
+		return rule.SingleCheckResult(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget())), nil
+	}
+	selector := labels.NewSelector().Add(*notDikiPodReq)
+
+	for _, namespace := range systemNamespaces {
+		podsPartialMetadata, err := kubeutils.GetObjectsMetadata(ctx, r.Client, corev1.SchemeGroupVersion.WithKind("PodList"), namespace, selector, 300)
+		if err != nil {
+			return rule.SingleCheckResult(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("namespace", namespace, "kind", "podList"))), nil
+		}
+
+		for _, podPartialMetadata := range podsPartialMetadata {
+			target := rule.NewTarget("name", podPartialMetadata.Name, "namespace", podPartialMetadata.Namespace, "kind", "pod")
+
+			acceptedPodIdx := slices.IndexFunc(acceptedPods, func(acceptedPod AcceptedPods242417) bool {
+				return (len(acceptedPod.NamespaceNames) == 0 || slices.Contains(acceptedPod.NamespaceNames, namespace)) &&
+					utils.MatchLabels(podPartialMetadata.Labels, acceptedPod.PodMatchLabels)
+			})
+
+			if acceptedPodIdx < 0 {
+				checkResults = append(checkResults, rule.FailedCheckResult("Found user pods in system namespaces.", target))
+				continue
+			}
+
+			acceptedPod := r.Options.AcceptedPods[acceptedPodIdx]
+
+			msg := acceptedPod.Justification
+			switch acceptedPod.Status {
+			case "Passed", "passed":
+				if len(msg) == 0 {
+					msg = "System pod in system namespaces."
+				}
+				checkResults = append(checkResults, rule.PassedCheckResult(msg, target))
+			case "Accepted", "accepted", "":
+				if len(msg) == 0 {
+					msg = "Accepted user pod in system namespaces."
+				}
+				checkResults = append(checkResults, rule.AcceptedCheckResult(msg, target))
+			default:
+				checkResults = append(checkResults, rule.ErroredCheckResult(fmt.Sprintf("unrecognized status set: %s", acceptedPod.Status), target))
+			}
+		}
+	}
+
+	return rule.RuleResult{
+		RuleID:       r.ID(),
+		RuleName:     r.Name(),
+		CheckResults: checkResults,
+	}, nil
+}
---
+++ pkg/shared/ruleset/disak8sstig/v1r11/242417_test.go
@@ -0,0 +1,178 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package v1r11_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/gardener/diki/pkg/rule"
+	"github.com/gardener/diki/pkg/shared/ruleset/disak8sstig/v1r11"
+)
+
+var _ = Describe("#242417", func() {
+	var (
+		fakeClient client.Client
+		plainPod   *corev1.Pod
+		options    *v1r11.Options242417
+		ctx        = context.TODO()
+	)
+
+	BeforeEach(func() {
+		fakeClient = fakeclient.NewClientBuilder().Build()
+		plainPod = &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "foo",
+				Labels:    map[string]string{},
+			},
+		}
+		options = &v1r11.Options242417{
+			AcceptedPods: []v1r11.AcceptedPods242417{
+				{
+					PodMatchLabels: map[string]string{},
+				},
+			},
+		}
+	})
+
+	It("should return passed checkResult when no user pods are present in system namespaces", func() {
+		pod1 := plainPod.DeepCopy()
+		pod1.Name = "pod1"
+		Expect(fakeClient.Create(ctx, pod1)).To(Succeed())
+
+		pod2 := plainPod.DeepCopy()
+		pod2.Name = "bar"
+		pod2.Namespace = "kube-system"
+		pod2.Labels["label"] = "value"
+		Expect(fakeClient.Create(ctx, pod2)).To(Succeed())
+
+		pod3 := plainPod.DeepCopy()
+		pod3.Name = "bar"
+		pod3.Namespace = "kube-public"
+		pod3.Labels["label"] = "value"
+		Expect(fakeClient.Create(ctx, pod3)).To(Succeed())
+
+		pod4 := plainPod.DeepCopy()
+		pod4.Name = "bar"
+		pod4.Namespace = "kube-node-lease"
+		pod4.Labels["compliance.gardener.cloud/role"] = "diki-privileged-pod"
+		Expect(fakeClient.Create(ctx, pod4)).To(Succeed())
+
+		options.AcceptedPods[0].PodMatchLabels["label"] = "value"
+		options.AcceptedPods[0].Status = "Passed"
+		r := &v1r11.Rule242417{
+			Client:  fakeClient,
+			Options: options,
+			Logger:  testLogger,
+		}
+
+		ruleResult, err := r.Run(ctx)
+		Expect(err).To(BeNil())
+
+		expectedCheckResults := []rule.CheckResult{
+			rule.PassedCheckResult("System pod in system namespaces.", rule.NewTarget("name", "bar", "namespace", "kube-system", "kind", "pod")),
+			rule.PassedCheckResult("System pod in system namespaces.", rule.NewTarget("name", "bar", "namespace", "kube-public", "kind", "pod")),
+		}
+
+		Expect(ruleResult.CheckResults).To(ConsistOf(expectedCheckResults))
+	})
+
+	It("should return failed checkResult when user pods are present in system namespaces", func() {
+		pod1 := plainPod.DeepCopy()
+		pod1.Name = "foo"
+		pod1.Namespace = "kube-system"
+		Expect(fakeClient.Create(ctx, pod1)).To(Succeed())
+
+		pod2 := plainPod.DeepCopy()
+		pod2.Name = "bar"
+		pod2.Namespace = "kube-public"
+		Expect(fakeClient.Create(ctx, pod2)).To(Succeed())
+
+		pod3 := plainPod.DeepCopy()
+		pod3.Name = "foobar"
+		pod3.Namespace = "kube-node-lease"
+		pod3.Labels["label"] = "gardener"
+		Expect(fakeClient.Create(ctx, pod3)).To(Succeed())
+
+		r := &v1r11.Rule242417{Logger: testLogger, Client: fakeClient}
+
+		ruleResult, err := r.Run(ctx)
+		Expect(err).To(BeNil())
+
+		expectedCheckResults := []rule.CheckResult{
+			rule.FailedCheckResult("Found user pods in system namespaces.", rule.NewTarget("name", pod1.Name, "namespace", pod1.Namespace, "kind", "pod")),
+			rule.FailedCheckResult("Found user pods in system namespaces.", rule.NewTarget("name", pod2.Name, "namespace", pod2.Namespace, "kind", "pod")),
+			rule.FailedCheckResult("Found user pods in system namespaces.", rule.NewTarget("name", pod3.Name, "namespace", pod3.Namespace, "kind", "pod")),
+		}
+
+		Expect(ruleResult.CheckResults).To(Equal(expectedCheckResults))
+	})
+
+	It("should return correct checkResult when different statuses are used", func() {
+		pod1 := plainPod.DeepCopy()
+		pod1.Name = "pod1"
+		pod1.Namespace = "kube-system"
+		pod1.Labels["foo-bar"] = "bar"
+		Expect(fakeClient.Create(ctx, pod1)).To(Succeed())
+
+		pod2 := plainPod.DeepCopy()
+		pod2.Name = "bar"
+		pod2.Namespace = "kube-system"
+		pod2.Labels["foo"] = "bar"
+		pod2.Labels["bar"] = "foo"
+		Expect(fakeClient.Create(ctx, pod2)).To(Succeed())
+
+		pod3 := plainPod.DeepCopy()
+		pod3.Name = "bar"
+		pod3.Namespace = "kube-public"
+		pod3.Labels["foo"] = "bar"
+		Expect(fakeClient.Create(ctx, pod3)).To(Succeed())
+
+		pod4 := plainPod.DeepCopy()
+		pod4.Name = "bar"
+		pod4.Namespace = "kube-node-lease"
+		pod4.Labels["compliance.gardener.cloud/role"] = "diki-privileged-pod"
+		Expect(fakeClient.Create(ctx, pod4)).To(Succeed())
+
+		options.AcceptedPods[0].PodMatchLabels["foo"] = "bar"
+		options.AcceptedPods[0].PodMatchLabels["bar"] = "foo"
+		options.AcceptedPods[0].Status = "Accepted"
+		options.AcceptedPods[0].Justification = "Accept pod."
+		options.AcceptedPods[0].NamespaceNames = []string{"kube-system"}
+		options.AcceptedPods = append(options.AcceptedPods, v1r11.AcceptedPods242417{
+			PodMatchLabels: map[string]string{
+				"foo": "bar",
+			},
+		})
+		options.AcceptedPods = append(options.AcceptedPods, v1r11.AcceptedPods242417{
+			PodMatchLabels: map[string]string{
+				"foo-bar": "bar",
+			},
+			Status: "fake",
+		})
+		r := &v1r11.Rule242417{
+			Client:  fakeClient,
+			Options: options,
+			Logger:  testLogger,
+		}
+
+		ruleResult, err := r.Run(ctx)
+		Expect(err).To(BeNil())
+
+		expectedCheckResults := []rule.CheckResult{
+			rule.ErroredCheckResult("unrecognized status set: fake", rule.NewTarget("name", "pod1", "namespace", "kube-system", "kind", "pod")),
+			rule.AcceptedCheckResult("Accept pod.", rule.NewTarget("name", "bar", "namespace", "kube-system", "kind", "pod")),
+			rule.AcceptedCheckResult("Accepted user pod in system namespaces.", rule.NewTarget("name", "bar", "namespace", "kube-public", "kind", "pod")),
+		}
+
+		Expect(ruleResult.CheckResults).To(ConsistOf(expectedCheckResults))
+	})
+})
