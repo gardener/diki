f501260179b979a4a8ffd06684eeae5931f2410f
Dimitar Mirchev <dimitar.mirchev@sap.com>
2024-02-29 16:50:10 +0200

    Some minor refactoring

--- pkg/provider/gardener/ruleset/disak8sstig/v1r11/254801.go
+++ pkg/provider/gardener/ruleset/disak8sstig/v1r11/254801.go
@@ -47,32 +47,25 @@ func (r *Rule254801) Run(ctx context.Context) (rule.RuleResult, error) {
 	if r.ControlPlaneVersion != nil && versionutils.ConstraintK8sGreaterEqual128.Check(r.ControlPlaneVersion) {
 		checkResults = append(checkResults, rule.SkippedCheckResult(fmt.Sprintf("Option %s removed in Kubernetes v1.28.", option), seedTarget.With("details", fmt.Sprintf("Used Kubernetes version %s.", r.ControlPlaneVersion.String()))))
 	} else {
-		const (
-			kapiName = "kube-apiserver"
-			kcmName  = "kube-controller-manager"
-			ksName   = "kube-scheduler"
-		)
-		deploymentNames := []string{kapiName, kcmName, ksName}
-
+		deploymentNames := []string{"kube-apiserver", "kube-controller-manager", "kube-scheduler"}
 		for _, deploymentName := range deploymentNames {
 			target := seedTarget.With("name", deploymentName, "namespace", r.ControlPlaneNamespace, "kind", "deployment")
-			fgOptSlice, err := kubeutils.GetCommandOptionFromDeployment(ctx, r.ControlPlaneClient, deploymentName, deploymentName, r.ControlPlaneNamespace, "feature-gates")
+			options, err := kubeutils.GetCommandOptionFromDeployment(ctx, r.ControlPlaneClient, deploymentName, deploymentName, r.ControlPlaneNamespace, "feature-gates")
 			if err != nil {
 				checkResults = append(checkResults, rule.ErroredCheckResult(err.Error(), seedTarget))
 				continue
 			}
 
-			PodSecurityOptSlice := kubeutils.FindInnerValue(fgOptSlice, "PodSecurity")
-
+			podSecurityOptions := kubeutils.FindInnerValue(options, "PodSecurity")
 			// featureGates.PodSecurity defaults to true in versions >= v1.23. ref https://kubernetes.io/docs/reference/command-line-tools-reference/feature-gates/#feature-gates-for-alpha-or-beta-features
 			switch {
-			case len(PodSecurityOptSlice) == 0:
+			case len(podSecurityOptions) == 0:
 				checkResults = append(checkResults, rule.PassedCheckResult(fmt.Sprintf("Option %s not set.", option), target))
-			case len(PodSecurityOptSlice) > 1:
+			case len(podSecurityOptions) > 1:
 				checkResults = append(checkResults, rule.WarningCheckResult(fmt.Sprintf("Option %s has been set more than once in container command.", option), target))
-			case PodSecurityOptSlice[0] == "true":
+			case podSecurityOptions[0] == "true":
 				checkResults = append(checkResults, rule.PassedCheckResult(fmt.Sprintf("Option %s set to allowed value.", option), target))
-			case PodSecurityOptSlice[0] == "false":
+			case podSecurityOptions[0] == "false":
 				checkResults = append(checkResults, rule.FailedCheckResult(fmt.Sprintf("Option %s set to not allowed value.", option), target))
 			default:
 				checkResults = append(checkResults, rule.WarningCheckResult(fmt.Sprintf("Option %s set to neither 'true' nor 'false'.", option), target))
@@ -86,7 +79,6 @@ func (r *Rule254801) Run(ctx context.Context) (rule.RuleResult, error) {
 	if r.ClusterVersion != nil && versionutils.ConstraintK8sGreaterEqual128.Check(r.ClusterVersion) {
 		checkResults = append(checkResults, rule.SkippedCheckResult(fmt.Sprintf("Option %s removed in Kubernetes v1.28.", option), shootTarget.With("details", fmt.Sprintf("Used Kubernetes version %s.", r.ClusterVersion.String()))))
 	} else {
-
 		nodes, err := kubeutils.GetNodes(ctx, r.ClusterClient, 300)
 		if err != nil {
 			return rule.RuleResult{
