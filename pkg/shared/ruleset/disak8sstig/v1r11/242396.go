18eca6f787ef39fbed70192e5f074a5244ac0eec
Aleksandar Savchev <aleksandar.savchev@sap.com>
2024-03-06 13:45:12 +0200

    Add suggested changes

--- pkg/shared/ruleset/disak8sstig/v1r11/242396.go
+++ pkg/shared/ruleset/disak8sstig/v1r11/242396.go
@@ -16,10 +16,10 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/component-base/version"
+	kubectlversion "k8s.io/kubectl/pkg/cmd/version"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/gardener/diki/imagevector"
-	"github.com/gardener/diki/pkg/kubernetes/config"
 	"github.com/gardener/diki/pkg/kubernetes/pod"
 	kubeutils "github.com/gardener/diki/pkg/kubernetes/utils"
 	"github.com/gardener/diki/pkg/rule"
@@ -82,70 +82,18 @@ func (r *Rule242396) Run(ctx context.Context) (rule.RuleResult, error) {
 	}
 	image.WithOptionalTag(version.Get().GitVersion)
 
+	constraintK8s, err := semver.NewConstraint("< 1.12.9")
+	if err != nil {
+		return rule.SingleCheckResult(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget())), nil
+	}
+
 	slices.SortFunc(selectedNodes, func(n1, n2 corev1.Node) int {
 		return cmp.Compare(n1.Name, n2.Name)
 	})
 
 	for _, node := range selectedNodes {
-		var (
-			kubectlVersion   config.KubectlVersion
-			podName          = fmt.Sprintf("diki-%s-%s", r.ID(), Generator.Generate(10))
-			nodeTarget       = rule.NewTarget("kind", "node", "name", node.Name)
-			execPodTarget    = rule.NewTarget("name", podName, "namespace", "kube-system", "kind", "pod")
-			additionalLabels = map[string]string{pod.LabelInstanceID: r.InstanceID}
-		)
-
-		defer func() {
-			if err := r.PodContext.Delete(ctx, podName, "kube-system"); err != nil {
-				r.Logger.Error(err.Error())
-			}
-		}()
-
-		podExecutor, err := r.PodContext.Create(ctx, pod.NewPrivilegedPod(podName, "kube-system", image.String(), node.Name, additionalLabels))
-		if err != nil {
-			checkResults = append(checkResults, rule.ErroredCheckResult(err.Error(), execPodTarget))
-			continue
-		}
-
-		commandResult, err := podExecutor.Execute(ctx, "/bin/sh", `kubectl version --client --output=json`)
-		if err != nil {
-			if strings.Contains(err.Error(), "command terminated with exit code 127") {
-				checkResults = append(checkResults, rule.SkippedCheckResult("Kubectl command could not be found (or not installed)", nodeTarget))
-			} else {
-				checkResults = append(checkResults, rule.ErroredCheckResult(err.Error(), execPodTarget))
-			}
-			continue
-		}
-
-		err = json.Unmarshal([]byte(commandResult), &kubectlVersion)
-		if err != nil {
-			checkResults = append(checkResults, rule.ErroredCheckResult(err.Error(), nodeTarget))
-			continue
-		}
-
-		if len(kubectlVersion.ClientVersion.GitVersion) == 0 {
-			checkResults = append(checkResults, rule.ErroredCheckResult("kubectl client version not preset in output", execPodTarget.With("output", commandResult)))
-			continue
-		}
-
-		clientVersion, err := semver.NewVersion(kubectlVersion.ClientVersion.GitVersion)
-		if err != nil {
-			checkResults = append(checkResults, rule.ErroredCheckResult(err.Error(), nodeTarget))
-			continue
-		}
-
-		constraintK8s, err := semver.NewConstraint("< 1.12.9")
-		if err != nil {
-			checkResults = append(checkResults, rule.ErroredCheckResult(err.Error(), nodeTarget))
-			continue
-		}
-
-		if constraintK8s.Check(clientVersion) {
-			checkResults = append(checkResults, rule.FailedCheckResult("Node uses not allowed kubectl version", nodeTarget.With("details", fmt.Sprintf("Kubectl client version %s", clientVersion.String()))))
-			continue
-		}
-
-		checkResults = append(checkResults, rule.PassedCheckResult("Node uses allowed kubectl version", nodeTarget.With("details", fmt.Sprintf("Kubectl client version %s", clientVersion.String()))))
+		checkResults = append(checkResults,
+			r.checkKubectl(ctx, node.Name, image.String(), constraintK8s))
 	}
 
 	return rule.RuleResult{
@@ -153,4 +101,58 @@ func (r *Rule242396) Run(ctx context.Context) (rule.RuleResult, error) {
 		RuleName:     r.Name(),
 		CheckResults: checkResults,
 	}, nil
+}
+
+func (r *Rule242396) checkKubectl(
+	ctx context.Context,
+	nodeName, imageName string,
+	constraintK8s *semver.Constraints,
+) rule.CheckResult {
+	var (
+		kubectlVersion   kubectlversion.Version
+		podName          = fmt.Sprintf("diki-%s-%s", r.ID(), Generator.Generate(10))
+		nodeTarget       = rule.NewTarget("kind", "node", "name", nodeName)
+		execPodTarget    = rule.NewTarget("name", podName, "namespace", "kube-system", "kind", "pod")
+		additionalLabels = map[string]string{pod.LabelInstanceID: r.InstanceID}
+	)
+
+	defer func() {
+		if err := r.PodContext.Delete(ctx, podName, "kube-system"); err != nil {
+			r.Logger.Error(err.Error())
+		}
+	}()
+
+	podExecutor, err := r.PodContext.Create(ctx, pod.NewPrivilegedPod(podName, "kube-system", imageName, nodeName, additionalLabels))
+	if err != nil {
+		return rule.ErroredCheckResult(err.Error(), execPodTarget)
+	}
+
+	commandResult, err := podExecutor.Execute(ctx, "/bin/sh", `kubectl version --client --output=json`)
+	if err != nil {
+		if strings.Contains(err.Error(), "command terminated with exit code 127") {
+			return rule.SkippedCheckResult("Kubectl command could not be found (or not installed)", nodeTarget)
+		}
+
+		return rule.ErroredCheckResult(err.Error(), execPodTarget)
+	}
+
+	err = json.Unmarshal([]byte(commandResult), &kubectlVersion)
+	if err != nil {
+		return rule.ErroredCheckResult(err.Error(), nodeTarget)
+	}
+
+	if kubectlVersion.ClientVersion == nil || len(kubectlVersion.ClientVersion.GitVersion) == 0 {
+		return rule.ErroredCheckResult("kubectl client version not preset in output", execPodTarget.With("output", commandResult))
+	}
+
+	clientVersion, err := semver.NewVersion(kubectlVersion.ClientVersion.GitVersion)
+	if err != nil {
+		return rule.ErroredCheckResult(err.Error(), nodeTarget)
+	}
+
+	if constraintK8s.Check(clientVersion) {
+		return rule.FailedCheckResult("Node uses not allowed kubectl version", nodeTarget.With("details", fmt.Sprintf("Kubectl client version %s", clientVersion.String())))
+	}
+
+	return rule.PassedCheckResult("Node uses allowed kubectl version", nodeTarget.With("details", fmt.Sprintf("Kubectl client version %s", clientVersion.String())))
 }
