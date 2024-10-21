7f988415f8f80f270a47b7fd7f4b755b2e71bb55
georgibaltiev <georgi.baltiev@sap.com>
2024-10-21 18:41:21 +0300

    Add an additional test for rule 242447 in order to validate different container name configurations for proxy pods

--- pkg/shared/ruleset/disak8sstig/rules/242447_test.go
+++ pkg/shared/ruleset/disak8sstig/rules/242447_test.go
@@ -166,6 +166,56 @@ var _ = Describe("#242447", func() {
 		}))
 	})
 
+	It("should correctly check if container names are in the valid specifications", func() {
+		Expect(fakeClient.Create(ctx, Node)).To(Succeed())
+		Expect(fakeClient.Create(ctx, dikiPod)).To(Succeed())
+
+		kubeProxyContainerPod := plainPod.DeepCopy()
+		kubeProxyContainerPod.Name = "kube-proxy-container-pod"
+		kubeProxyContainerPod.Labels["role"] = "proxy"
+		kubeProxyContainerPod.Spec.Containers[0].Command = []string{"--config=/var/lib/config", "--kubeconfig=/var/lib/kubeconfig"}
+		kubeProxyContainerPod.OwnerReferences[0].UID = "0"
+		Expect(fakeClient.Create(ctx, kubeProxyContainerPod)).To(Succeed())
+
+		proxyContainerPod := plainPod.DeepCopy()
+		proxyContainerPod.Name = "proxy-container-pod"
+		proxyContainerPod.Labels["role"] = "proxy"
+		proxyContainerPod.Spec.Containers[0].Name = "proxy"
+		proxyContainerPod.Spec.Containers[0].Command = []string{"--config=/var/lib/config"}
+		proxyContainerPod.OwnerReferences[0].UID = "1"
+		Expect(fakeClient.Create(ctx, proxyContainerPod)).To(Succeed())
+
+		nonValidContainerPod := plainPod.DeepCopy()
+		nonValidContainerPod.Name = "non-valid-container-pod"
+		nonValidContainerPod.Labels["role"] = "proxy"
+		nonValidContainerPod.Spec.Containers[0].Name = "foo"
+		nonValidContainerPod.Spec.Containers[0].Command = []string{"--kubeconfig=/var/lib/kubeconfig"}
+		nonValidContainerPod.OwnerReferences[0].UID = "2"
+		Expect(fakeClient.Create(ctx, nonValidContainerPod)).To(Succeed())
+
+		fakePodContext = fakepod.NewFakeSimplePodContext([][]string{{mounts, compliantConfigStats, compliantKubeconfigStats, mounts, nonCompliantConfigStats, kubeProxyConfig, nonCompliantKubeconfigStats2}},
+			[][]error{{nil, nil, nil, nil, nil, nil, nil}})
+		r := &rules.Rule242447{
+			Logger:     testLogger,
+			InstanceID: instanceID,
+			Client:     fakeClient,
+			PodContext: fakePodContext,
+		}
+
+		ruleResult, err := r.Run(ctx)
+		Expect(err).To(BeNil())
+
+		expectedResults := []rule.CheckResult{
+			rule.PassedCheckResult("File has expected permissions", rule.NewTarget("name", kubeProxyContainerPod.Name, "namespace", kubeProxyContainerPod.Namespace, "kind", "pod", "details", "fileName: /var/lib/config, permissions: 644")),
+			rule.PassedCheckResult("File has expected permissions", rule.NewTarget("name", kubeProxyContainerPod.Name, "namespace", kubeProxyContainerPod.Namespace, "kind", "pod", "details", "fileName: /var/lib/kubeconfig, permissions: 600")),
+			rule.ErroredCheckResult("pod does not contain any of the containers specified in the provided list: [kube-proxy proxy]", rule.NewTarget("name", nonValidContainerPod.Name, "namespace", nonValidContainerPod.Namespace, "kind", "pod")),
+			rule.FailedCheckResult("File has too wide permissions", rule.NewTarget("name", proxyContainerPod.Name, "namespace", proxyContainerPod.Namespace, "kind", "pod", "details", "fileName: /var/lib/config, permissions: 664, expectedPermissionsMax: 644")),
+			rule.FailedCheckResult("File has too wide permissions", rule.NewTarget("name", proxyContainerPod.Name, "namespace", proxyContainerPod.Namespace, "kind", "pod", "details", "fileName: /var/lib/kubeconfig2, permissions: 606, expectedPermissionsMax: 644")),
+		}
+
+		Expect(ruleResult.CheckResults).To(Equal(expectedResults))
+	})
+
 	DescribeTable("Run cases",
 		func(options rules.Options242447, executeReturnString [][]string, executeReturnError [][]error, expectedCheckResults []rule.CheckResult) {
 			Expect(fakeClient.Create(ctx, Node)).To(Succeed())
