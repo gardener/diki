e8f4f632c957e31630827746b81045f95052dd7e
georgibaltiev <georgi.baltiev@sap.com>
2024-10-24 16:10:13 +0300

    Refactor the implementation of 242390

--- pkg/provider/managedk8s/ruleset/disak8sstig/rules/242390.go
+++ pkg/provider/managedk8s/ruleset/disak8sstig/rules/242390.go
@@ -2,21 +2,18 @@ package rules
 
 import (
 	"context"
-
-	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"crypto/tls"
+	"net/http"
 
 	"github.com/gardener/diki/pkg/rule"
-
 	sharedrules "github.com/gardener/diki/pkg/shared/ruleset/disak8sstig/rules"
 )
 
 var _ rule.Rule = &Rule242390{}
 
 type Rule242390 struct {
-	InstanceID   string
-	Client       client.Client
-	V1RESTClient rest.Interface
+	InstanceID      string
+	KAPIExternalURL string
 }
 
 func (r *Rule242390) ID() string {
@@ -28,9 +25,26 @@ func (r *Rule242390) Name() string {
 }
 
 func (r *Rule242390) Run(ctx context.Context) (rule.RuleResult, error) {
+	transportConfiguration := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
 
-	//retrieve URL to the kube-api server which is guarded
+	externalClient := &http.Client{
+		Transport: transportConfiguration,
+	}
 
-	//parse HttpResponse from the Get(url)
+	httpResponse, err := externalClient.Get(r.KAPIExternalURL)
+	if err != nil {
+		return rule.SingleCheckResult(r, rule.ErroredCheckResult("failed http request"+err.Error(), rule.NewTarget())), nil
+	}
 
+	if httpResponse.StatusCode == http.StatusUnauthorized {
+		return rule.SingleCheckResult(r, rule.FailedCheckResult("kube-apiserver has anonymous authentication enabled", rule.NewTarget())), nil
+	} else if httpResponse.StatusCode == http.StatusForbidden {
+		return rule.SingleCheckResult(r, rule.PassedCheckResult("kube-apiserver has anonymous authentication disabled", rule.NewTarget())), nil
+	} else {
+		return rule.SingleCheckResult(r, rule.WarningCheckResult("the anonymous authentication configurations on the kube-apiserver can not be determined", rule.NewTarget())), nil
+	}
 }
