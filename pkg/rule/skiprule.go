1bd2309400ca4fa757af7732284d1eaffcf6b845
georgibaltiev <georgi.baltiev@sap.com>
2024-11-12 18:08:30 +0200

    Refactor SkipRule to use severity and implement the Severity interface

--- pkg/rule/skiprule.go
+++ pkg/rule/skiprule.go
@@ -9,20 +9,23 @@ import (
 )
 
 var _ Rule = &SkipRule{}
+var _ Severity = &SkipRule{}
 
 // SkipRule is a Rule that always reports a predefined status.
 type SkipRule struct {
 	id            string
 	name          string
+	severity      SeverityLevel
 	justification string
 	status        Status
 }
 
 // NewSkipRule returns a new skipped Rule.
-func NewSkipRule(id, name, justification string, status Status) *SkipRule {
+func NewSkipRule(id, name, justification string, status Status, severity ...SeverityLevel) *SkipRule {
 	return &SkipRule{
 		id:            id,
 		name:          name,
+		severity:      severity[0],
 		justification: justification,
 		status:        status,
 	}
@@ -36,6 +39,11 @@ func (s *SkipRule) ID() string {
 // Name returns the name of the Rule.
 func (s *SkipRule) Name() string {
 	return s.name
+}
+
+// Severity returns the severity level of the Rule
+func (s *SkipRule) Severity() SeverityLevel {
+	return s.severity
 }
 
 // Run immediately returns a RuleResult containing
