e335abf3bf135217a38c896dee35d715eb01a6c2
georgibaltiev <georgi.baltiev@sap.com>
2024-11-19 11:47:03 +0200

    Add shoot check

--- pkg/provider/garden/ruleset/securityhardenedshoot/rules/2006.go
+++ pkg/provider/garden/ruleset/securityhardenedshoot/rules/2006.go
@@ -7,7 +7,10 @@ package rules
 import (
 	"context"
 
+	"github.com/docker/docker-credential-helpers/client"
 	"github.com/gardener/diki/pkg/rule"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var (
@@ -16,6 +19,9 @@ var (
 )
 
 type Rule2006 struct {
+	Client         client.Client
+	ShootName      string
+	ShootNamespace string
 }
 
 func (r *Rule2006) ID() string {
@@ -31,5 +37,13 @@ func (r *Rule2006) Severity() rule.SeverityLevel {
 }
 
 func (r *Rule2006) Run(ctx context.Context) (rule.RuleResult, error) {
+	shoot := &gardencorev1beta1.Shoot{ObjectMeta: metav1.ObjectMeta{Name: r.ShootName, Namespace: r.ShootNamespace}}
+	if err := client.Get(ctx, client.GetObjectFromKey(shoot), shoot); err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("name", r.ShootName, "namespace", r.ShootNamespace, "kind", "Shoot"))), nil
+	}
+
+	if shoot.Spec.Kubernetes.EnableStaticTokenKubeconfig == nil {
+
+	}
 
 }
