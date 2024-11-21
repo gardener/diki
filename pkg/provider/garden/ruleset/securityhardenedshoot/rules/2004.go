156f6a3747f2ff6e7bd7287f0518358d3189524c
georgibaltiev <georgi.baltiev@sap.com>
2024-11-21 14:39:48 +0200

    Simplify code

--- pkg/provider/garden/ruleset/securityhardenedshoot/rules/2004.go
+++ pkg/provider/garden/ruleset/securityhardenedshoot/rules/2004.go
@@ -52,19 +52,15 @@ func (r *Rule2004) Run(ctx context.Context) (rule.RuleResult, error) {
 
 	// The ValidatingadmissionWebhook is required to be enabled by Gardener. ref: https://github.com/gardener/gardener/blob/834a3476ed4343225690cd0eaca32e03ab5b84f5/pkg/utils/validation/admissionplugins/admissionplugins.go#L71
 	// Therefore, this rule check is always expected to pass.
-	enabledIdx := slices.IndexFunc(admissionPlugins, func(plugin gardencorev1beta1.AdmissionPlugin) bool {
-		return plugin.Name == "ValidatingAdmissionWebhook" && plugin.Disabled != nil && !*plugin.Disabled
-	})
-	disabledIdx := slices.IndexFunc(admissionPlugins, func(plugin gardencorev1beta1.AdmissionPlugin) bool {
+	if slices.ContainsFunc(admissionPlugins, func(plugin gardencorev1beta1.AdmissionPlugin) bool {
 		return plugin.Name == "ValidatingAdmissionWebhook" && plugin.Disabled != nil && *plugin.Disabled
-	})
-
-	if disabledIdx >= 0 {
+	}) {
 		return rule.Result(r, rule.FailedCheckResult("The ValidatingAdmissionWebhook admission plugin is disabled.", rule.NewTarget())), nil
 	}
-	if enabledIdx >= 0 {
+	if slices.ContainsFunc(admissionPlugins, func(plugin gardencorev1beta1.AdmissionPlugin) bool {
+		return plugin.Name == "ValidatingAdmissionWebhook" && plugin.Disabled != nil && !*plugin.Disabled
+	}) {
 		return rule.Result(r, rule.PassedCheckResult("The ValidatingAdmissionWebhook admission plugin is enabled.", rule.NewTarget())), nil
 	}
-
 	return rule.Result(r, rule.PassedCheckResult("The ValidatingAdmissionWebhook admission plugin is not disabled.", rule.NewTarget())), nil
 }
