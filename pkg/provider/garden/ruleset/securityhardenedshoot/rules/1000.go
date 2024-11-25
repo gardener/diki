1de7055d2477c518af71f6d69e8ef8fee6cafceb
georgibaltiev <georgi.baltiev@sap.com>
2024-11-25 15:23:03 +0200

    Refactor options for rule 1000 and add unit tests

--- pkg/provider/garden/ruleset/securityhardenedshoot/rules/1000.go
+++ pkg/provider/garden/ruleset/securityhardenedshoot/rules/1000.go
@@ -9,12 +9,13 @@ import (
 	"fmt"
 	"slices"
 
-	"github.com/gardener/diki/pkg/rule"
-	"github.com/gardener/diki/pkg/shared/ruleset/disak8sstig/option"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/gardener/diki/pkg/rule"
+	"github.com/gardener/diki/pkg/shared/ruleset/disak8sstig/option"
 )
 
 var (
@@ -23,13 +24,26 @@ var (
 	_ option.Option = &Options1000{}
 )
 
+type Extension1000 struct {
+	Type string `json:"type" yaml:"type"`
+}
+
 type Options1000 struct {
-	Types []string `json:"minPodSecurityLevel" yaml:"minPodSecurityLevel"`
+	Extensions []Extension1000 `json:"extensions" yaml:"extensions"`
 }
 
 func (o Options1000) Validate() field.ErrorList {
-	// Is there even anything to validate here?
-	return nil
+	var (
+		allErrs  = field.ErrorList{}
+		rootPath = field.NewPath("extensions")
+	)
+
+	for _, extension := range o.Extensions {
+		if len(extension.Type) == 0 {
+			allErrs = append(allErrs, field.Required(rootPath.Child("type"), "must not be empty"))
+		}
+	}
+	return allErrs
 }
 
 type Rule1000 struct {
@@ -53,28 +67,30 @@ func (r *Rule1000) Severity() rule.SeverityLevel {
 
 func (r *Rule1000) Run(ctx context.Context) (rule.RuleResult, error) {
 
-	if r.Options == nil {
-		return rule.Result(r, rule.PassedCheckResult("The shoot cluster has all required extensions enabled.", rule.NewTarget())), nil
-	}
-
 	shoot := &gardencorev1beta1.Shoot{ObjectMeta: v1.ObjectMeta{Name: r.ShootName, Namespace: r.ShootNamespace}}
 	if err := r.Client.Get(ctx, client.ObjectKeyFromObject(shoot), shoot); err != nil {
 		return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("name", r.ShootName, "namespace", r.ShootNamespace, "kind", "Shoot"))), nil
 	}
 
+	if r.Options == nil {
+		return rule.Result(r, rule.PassedCheckResult("The shoot cluster has all required extensions enabled.", rule.NewTarget())), nil
+	}
+
 	var checkResults = []rule.CheckResult{}
 
-	for _, extensionTypeFromOption := range r.Options.Types {
+	for _, extensionTypeFromOption := range r.Options.Extensions {
 		extensionTypeIndex := slices.IndexFunc(shoot.Spec.Extensions, func(extension gardencorev1beta1.Extension) bool {
-			return extension.Type == extensionTypeFromOption && extension.Disabled == nil || !(*extension.Disabled)
+			return extension.Type == extensionTypeFromOption.Type
 		})
 
 		if extensionTypeIndex < 0 {
-			checkResults = append(checkResults, rule.FailedCheckResult(fmt.Sprintf("Extension %s is not configured for the shoot cluster.", extensionTypeFromOption), rule.NewTarget()))
-		} else if shoot.Spec.Extensions[extensionTypeIndex].Disabled == nil || !*(shoot.Spec.Extensions[extensionTypeIndex].Disabled) {
-			checkResults = append(checkResults, rule.AcceptedCheckResult(fmt.Sprintf("Extension %s is enabled for the shoot cluster.", extensionTypeFromOption), rule.NewTarget()))
+			checkResults = append(checkResults, rule.FailedCheckResult(fmt.Sprintf("Extension %s is not configured for the shoot cluster.", extensionTypeFromOption.Type), rule.NewTarget()))
 		} else {
-			checkResults = append(checkResults, rule.FailedCheckResult(fmt.Sprintf("Extension %s is disabled for the shoot cluster.", extensionTypeFromOption), rule.NewTarget()))
+			if shoot.Spec.Extensions[extensionTypeIndex].Disabled == nil || !*(shoot.Spec.Extensions[extensionTypeIndex].Disabled) {
+				checkResults = append(checkResults, rule.PassedCheckResult(fmt.Sprintf("Extension %s is enabled for the shoot cluster.", extensionTypeFromOption.Type), rule.NewTarget()))
+			} else {
+				checkResults = append(checkResults, rule.FailedCheckResult(fmt.Sprintf("Extension %s is disabled for the shoot cluster.", extensionTypeFromOption.Type), rule.NewTarget()))
+			}
 		}
 	}
 
--- pkg/provider/garden/ruleset/securityhardenedshoot/rules/1000_test.go
+++ pkg/provider/garden/ruleset/securityhardenedshoot/rules/1000_test.go
@@ -7,23 +7,25 @@ package rules_test
 import (
 	"context"
 
-	"github.com/gardener/diki/pkg/provider/garden/ruleset/securityhardenedshoot/rules"
-	"github.com/gardener/diki/pkg/rule"
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	"github.com/gardener/gardener/pkg/client/kubernetes"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/gardener/diki/pkg/provider/garden/ruleset/securityhardenedshoot/rules"
+	"github.com/gardener/diki/pkg/rule"
 )
 
 var _ = Describe("#1000", func() {
 	var (
-		fakeClient = client.Client
+		fakeClient client.Client
 		ctx        = context.TODO()
 
-		shoot *gardencorev1beta.Shoot
+		shoot *gardencorev1beta1.Shoot
 
 		shootName      = "foo"
 		shootNamespace = "bar"
@@ -31,7 +33,7 @@ var _ = Describe("#1000", func() {
 		r        rule.Rule
 		ruleID   = "1000"
 		ruleName = "Shoot clusters should enable required extensions. This rule can be configured as per organisation's requirements in order to check if required extensions are enabled for the shoot cluster."
-		severity = rule.SeverityHigh
+		severity = rule.SeverityMedium
 	)
 
 	BeforeEach(func() {
@@ -55,5 +57,131 @@ var _ = Describe("#1000", func() {
 		Expect(err).To(BeNil())
 		Expect(res).To(Equal(rule.RuleResult{RuleID: ruleID, RuleName: ruleName, Severity: severity, CheckResults: expectedCheckResults}))
 	},
-		Entry())
+		Entry("should error when the shoot can't be found",
+			func() { shoot.Name = "notFoo" },
+			nil,
+			[]rule.CheckResult{
+				{Status: rule.Errored, Message: "shoots.core.gardener.cloud \"foo\" not found", Target: rule.NewTarget("name", "foo", "namespace", "bar", "kind", "Shoot")},
+			},
+		),
+		Entry("should return a passed check result if there are no expected extensions to check",
+			func() {},
+			nil,
+			[]rule.CheckResult{
+				{Status: rule.Passed, Message: "The shoot cluster has all required extensions enabled.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should fail when a listed extension cannot be found",
+			func() {
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type: "foo",
+					},
+				}
+			},
+			&rules.Options1000{Extensions: []rules.Extension1000{
+				{
+					Type: "not-foo",
+				},
+			},
+			},
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "Extension not-foo is not configured for the shoot cluster.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should fail when a listed extension is explicitly disabled",
+			func() {
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type:     "foo",
+						Disabled: ptr.To(true),
+					},
+				}
+			},
+			&rules.Options1000{Extensions: []rules.Extension1000{
+				{
+					Type: "foo",
+				},
+			},
+			},
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "Extension foo is disabled for the shoot cluster.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should pass when a listed extension has default disabling",
+			func() {
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type: "foo",
+					},
+				}
+			},
+			&rules.Options1000{Extensions: []rules.Extension1000{
+				{
+					Type: "foo",
+				},
+			},
+			},
+			[]rule.CheckResult{
+				{Status: rule.Passed, Message: "Extension foo is enabled for the shoot cluster.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should pass when a listed extension has default disabling",
+			func() {
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type:     "foo",
+						Disabled: ptr.To(false),
+					},
+				}
+			},
+			&rules.Options1000{Extensions: []rules.Extension1000{
+				{
+					Type: "foo",
+				},
+			},
+			},
+			[]rule.CheckResult{
+				{Status: rule.Passed, Message: "Extension foo is enabled for the shoot cluster.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should create a check result for each provided extension in the configuration",
+			func() {
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type: "one",
+					},
+					{
+						Type:     "two",
+						Disabled: ptr.To(false),
+					},
+					{
+						Type:     "three",
+						Disabled: ptr.To(true),
+					},
+				}
+			},
+			&rules.Options1000{Extensions: []rules.Extension1000{
+				{
+					Type: "one",
+				},
+				{
+					Type: "two",
+				},
+				{
+					Type: "three",
+				},
+				{
+					Type: "four",
+				},
+			},
+			},
+			[]rule.CheckResult{
+				{Status: rule.Passed, Message: "Extension one is enabled for the shoot cluster.", Target: rule.NewTarget()},
+				{Status: rule.Passed, Message: "Extension two is enabled for the shoot cluster.", Target: rule.NewTarget()},
+				{Status: rule.Failed, Message: "Extension three is disabled for the shoot cluster.", Target: rule.NewTarget()},
+				{Status: rule.Failed, Message: "Extension four is not configured for the shoot cluster.", Target: rule.NewTarget()},
+			},
+		),
+	)
 })
