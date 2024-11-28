3d1d774c5d6f2bb877d60f09f5f9ee78a3d8dfbb
georgibaltiev <georgi.baltiev@sap.com>
2024-11-28 11:07:01 +0200

    Add implementation and test cases

--- pkg/provider/managedk8s/ruleset/securityhardenedk8s/rules/2002.go
+++ pkg/provider/managedk8s/ruleset/securityhardenedk8s/rules/2002.go
@@ -6,10 +6,14 @@ package rules
 
 import (
 	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	kubeutils "github.com/gardener/diki/pkg/kubernetes/utils"
 	"github.com/gardener/diki/pkg/rule"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 var (
@@ -34,7 +38,25 @@ func (r *Rule2002) Severity() rule.SeverityLevel {
 }
 
 func (r *Rule2002) Run(ctx context.Context) (rule.RuleResult, error) {
+	storageClasses, err := kubeutils.GetStorageClasses(ctx, r.Client, labels.NewSelector(), 300)
+	if err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("kind", "storageClassList"))), nil
+	}
 
-	kubeutils.GetPods(ctx, r.Client)
+	if len(storageClasses) == 0 {
+		return rule.Result(r, rule.PassedCheckResult("The cluster doesn't have any StorageClasses.", rule.NewTarget())), nil
+	}
 
+	var checkResults []rule.CheckResult
+
+	for _, storageClass := range storageClasses {
+		switch {
+		case storageClass.ReclaimPolicy == nil || *storageClass.ReclaimPolicy == corev1.PersistentVolumeReclaimDelete:
+			checkResults = append(checkResults, rule.PassedCheckResult("The StorageClass has a Delete ReclaimPolicy set.", rule.NewTarget("kind", "storageClass", "name", storageClass.Name)))
+		default:
+			checkResults = append(checkResults, rule.FailedCheckResult(fmt.Sprintf("The StorageClass has a %v ReclaimPolicy set.", *storageClass.ReclaimPolicy), rule.NewTarget("kind", "storageClass", "name", storageClass.Name)))
+		}
+	}
+
+	return rule.Result(r, checkResults...), err
 }
---
+++ pkg/provider/managedk8s/ruleset/securityhardenedk8s/rules/2002_test.go
@@ -0,0 +1,107 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package rules_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	storagev1 "k8s.io/api/storage/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/gardener/diki/pkg/provider/managedk8s/ruleset/securityhardenedk8s/rules"
+	"github.com/gardener/diki/pkg/rule"
+)
+
+var _ = Describe("#2002", func() {
+	var (
+		client            client.Client
+		plainStorageClass *storagev1.StorageClass
+
+		ctx = context.TODO()
+
+		ruleID   = "2002"
+		ruleName = "Storage Classes should have a \"Delete\" reclaim policy."
+		severity = rule.SeverityMedium
+	)
+
+	BeforeEach(func() {
+		client = fakeclient.NewClientBuilder().Build()
+		plainStorageClass = &storagev1.StorageClass{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "default",
+			},
+		}
+	})
+
+	DescribeTable("Run casees", func(updateFn func(), expectedCheckResults []rule.CheckResult) {
+		updateFn()
+
+		r := rules.Rule2002{Client: client}
+		res, err := r.Run(ctx)
+		Expect(err).To(BeNil())
+		Expect(res).To(Equal(rule.RuleResult{RuleID: ruleID, RuleName: ruleName, Severity: severity, CheckResults: expectedCheckResults}))
+	},
+		Entry("should pass when no storage classes are present",
+			func() {},
+			[]rule.CheckResult{
+				{Status: rule.Passed, Message: "The cluster doesn't have any StorageClasses.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should pass when a storage class's reclaim policy is default",
+			func() {
+				Expect(client.Create(ctx, plainStorageClass)).To(Succeed())
+			},
+			[]rule.CheckResult{
+				{Status: rule.Passed, Message: "The StorageClass has a Delete ReclaimPolicy set.", Target: rule.NewTarget("kind", "storageClass", "name", "default")},
+			},
+		),
+		Entry("should pass when a storage class's reclaim policy is explicitly set to delete",
+			func() {
+				plainStorageClass.ReclaimPolicy = ptr.To(corev1.PersistentVolumeReclaimDelete)
+				Expect(client.Create(ctx, plainStorageClass)).To(Succeed())
+			},
+			[]rule.CheckResult{
+				{Status: rule.Passed, Message: "The StorageClass has a Delete ReclaimPolicy set.", Target: rule.NewTarget("kind", "storageClass", "name", "default")},
+			},
+		),
+		Entry("should fail when a storage class's reclaim policy is set to a non-delete value",
+			func() {
+				plainStorageClass.ReclaimPolicy = ptr.To(corev1.PersistentVolumeReclaimRecycle)
+				Expect(client.Create(ctx, plainStorageClass)).To(Succeed())
+			},
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "The StorageClass has a Recycle ReclaimPolicy set.", Target: rule.NewTarget("kind", "storageClass", "name", "default")},
+			},
+		),
+		Entry("should return multiple check results when multiple storage classes are configured",
+			func() {
+				storageClassDefault := plainStorageClass.DeepCopy()
+				storageClassDefault.Name = "storageClassDefault"
+				Expect(client.Create(ctx, storageClassDefault)).To(Succeed())
+
+				storageClassDeletePolicy := plainStorageClass.DeepCopy()
+				storageClassDeletePolicy.Name = "storageClassDeletePolicyPolicy"
+				storageClassDeletePolicy.ReclaimPolicy = ptr.To(corev1.PersistentVolumeReclaimDelete)
+				Expect(client.Create(ctx, storageClassDeletePolicy)).To(Succeed())
+
+				storageClassRetainPolicy := plainStorageClass.DeepCopy()
+				storageClassRetainPolicy.Name = "storageClassRetainPolicyPolicy"
+				storageClassRetainPolicy.ReclaimPolicy = ptr.To(corev1.PersistentVolumeReclaimRetain)
+				Expect(client.Create(ctx, storageClassRetainPolicy)).To(Succeed())
+			},
+			[]rule.CheckResult{
+				{Status: rule.Passed, Message: "The StorageClass has a Delete ReclaimPolicy set.", Target: rule.NewTarget("kind", "storageClass", "name", "storageClassDefault")},
+				{Status: rule.Passed, Message: "The StorageClass has a Delete ReclaimPolicy set.", Target: rule.NewTarget("kind", "storageClass", "name", "storageClassDeletePolicyPolicy")},
+				{Status: rule.Failed, Message: "The StorageClass has a Retain ReclaimPolicy set.", Target: rule.NewTarget("kind", "storageClass", "name", "storageClassRetainPolicyPolicy")},
+			},
+		),
+	)
+})
