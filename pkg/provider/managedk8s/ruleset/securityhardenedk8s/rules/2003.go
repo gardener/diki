b940bc5b310c695cf0b754ebe09c30f91613abf4
Georgi Baltiev <82998942+georgibaltiev@users.noreply.github.com>
2024-12-09 14:58:24 +0200

    [Security Hardened Kubernetes Cluster] Rule 2003 implementation (#391)
    * Add Rule 2003
    
    * Add Rule 2003 implementation
    
    * Add Test cases
    
    * Add Options to Rule 2003
    
    * Add unit tests.
    
    * Add Options validation and rename
    
    * Fix namespace creation
    
    * Fix broken test case
    
    * Add suggestions
    
    * Improve rule
    
    ---------
    
    Co-authored-by: Dimitar Mirchev <dimitar.mirchev@sap.com>

---
+++ pkg/provider/managedk8s/ruleset/securityhardenedk8s/rules/2003.go
@@ -0,0 +1,130 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package rules
+
+import (
+	"context"
+	"slices"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/gardener/diki/pkg/internal/utils"
+	kubeutils "github.com/gardener/diki/pkg/kubernetes/utils"
+	"github.com/gardener/diki/pkg/rule"
+	"github.com/gardener/diki/pkg/shared/kubernetes/option"
+	disaoptions "github.com/gardener/diki/pkg/shared/ruleset/disak8sstig/option"
+)
+
+var (
+	_ rule.Rule          = &Rule2003{}
+	_ rule.Severity      = &Rule2003{}
+	_ disaoptions.Option = &Options2003{}
+)
+
+type Options2003 struct {
+	AcceptedPods []AcceptedPods2003 `json:"acceptedPods" yaml:"acceptedPods"`
+}
+
+type AcceptedPods2003 struct {
+	option.AcceptedNamespacedObject
+	VolumeNames []string `json:"volumeNames" yaml:"volumeNames"`
+}
+
+// Validate validates that option configurations are correctly defined
+func (o Options2003) Validate() field.ErrorList {
+	var (
+		allErrs  field.ErrorList
+		rootPath = field.NewPath("acceptedPods")
+	)
+	for _, p := range o.AcceptedPods {
+		allErrs = append(allErrs, p.Validate()...)
+		if len(p.VolumeNames) == 0 {
+			allErrs = append(allErrs, field.Required(rootPath.Child("volumeNames"), "must not be empty"))
+		}
+		for i, volumeName := range p.VolumeNames {
+			if len(volumeName) == 0 {
+				allErrs = append(allErrs, field.Invalid(rootPath.Child("volumeNames").Index(i), volumeName, "must not be empty"))
+			}
+		}
+	}
+	return allErrs
+}
+
+type Rule2003 struct {
+	Client  client.Client
+	Options *Options2003
+}
+
+func (r *Rule2003) ID() string {
+	return "2003"
+}
+
+func (r *Rule2003) Name() string {
+	return "Pods should use only allowed volume types."
+}
+
+func (r *Rule2003) Severity() rule.SeverityLevel {
+	return rule.SeverityMedium
+}
+
+func (r *Rule2003) Run(ctx context.Context) (rule.RuleResult, error) {
+	allNamespaces, err := kubeutils.GetNamespaces(ctx, r.Client)
+	if err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("kind", "namespaceList"))), nil
+	}
+
+	pods, err := kubeutils.GetPods(ctx, r.Client, "", labels.NewSelector(), 300)
+	if err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("kind", "podList"))), nil
+	}
+
+	var checkResults []rule.CheckResult
+	for _, pod := range pods {
+		uses := false
+		podTarget := rule.NewTarget("kind", "pod", "name", pod.Name, "namespace", pod.Namespace)
+		for _, volume := range pod.Spec.Volumes {
+			volumeTarget := podTarget.With("volume", volume.Name)
+			if volume.ConfigMap == nil &&
+				volume.CSI == nil &&
+				volume.DownwardAPI == nil &&
+				volume.EmptyDir == nil &&
+				volume.Ephemeral == nil &&
+				volume.PersistentVolumeClaim == nil &&
+				volume.Projected == nil &&
+				volume.Secret == nil {
+				uses = true
+				accepted, justification := r.accepted(volume, pod, allNamespaces[pod.Namespace])
+				if accepted {
+					checkResults = append(checkResults, rule.AcceptedCheckResult(justification, volumeTarget))
+				} else {
+					checkResults = append(checkResults, rule.FailedCheckResult("Pod uses not allowed volume type.", volumeTarget))
+				}
+			}
+		}
+		if !uses {
+			checkResults = append(checkResults, rule.PassedCheckResult("Pod uses only allowed volume types.", podTarget))
+		}
+	}
+	return rule.Result(r, checkResults...), nil
+}
+
+func (r *Rule2003) accepted(volume corev1.Volume, pod corev1.Pod, namespace corev1.Namespace) (bool, string) {
+	if r.Options == nil {
+		return false, ""
+	}
+
+	for _, acceptedPod := range r.Options.AcceptedPods {
+		if utils.MatchLabels(pod.Labels, acceptedPod.MatchLabels) && utils.MatchLabels(namespace.Labels, acceptedPod.NamespaceMatchLabels) {
+			if slices.Contains(acceptedPod.VolumeNames, volume.Name) {
+				return true, acceptedPod.Justification
+			}
+		}
+	}
+
+	return false, ""
+}
---
+++ pkg/provider/managedk8s/ruleset/securityhardenedk8s/rules/2003_test.go
@@ -0,0 +1,390 @@
+// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package rules_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gstruct"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/gardener/diki/pkg/provider/managedk8s/ruleset/securityhardenedk8s/rules"
+	"github.com/gardener/diki/pkg/rule"
+	"github.com/gardener/diki/pkg/shared/kubernetes/option"
+)
+
+var _ = Describe("#2003", func() {
+
+	var (
+		fakeClient    client.Client
+		plainPod      *corev1.Pod
+		ctx           = context.TODO()
+		namespaceName = "plainNamespace"
+		namespace     *corev1.Namespace
+		r             rules.Rule2003
+	)
+
+	BeforeEach(func() {
+		fakeClient = fakeclient.NewClientBuilder().Build()
+		namespace = &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: namespaceName,
+				Labels: map[string]string{
+					"foo": "bar",
+				},
+			},
+		}
+		plainPod = &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: namespaceName,
+				Labels: map[string]string{
+					"foo": "bar",
+				},
+			},
+		}
+	})
+
+	DescribeTable("Run cases", func(updatePodConfig func(), ruleOptions *rules.Options2003, expectedCheckResults []rule.CheckResult) {
+		Expect(fakeClient.Create(ctx, namespace)).To(Succeed())
+
+		updatePodConfig()
+
+		r = rules.Rule2003{Client: fakeClient, Options: ruleOptions}
+		result, err := r.Run(ctx)
+		Expect(err).To(BeNil())
+		Expect(result.CheckResults).To(Equal(expectedCheckResults))
+	},
+		Entry("should pass when all pod volumes are of an allowed type",
+			func() {
+				podWithPermittedVolumes := plainPod.DeepCopy()
+				podWithPermittedVolumes.Name = "podWithPermittedVolumes"
+				podWithPermittedVolumes.Spec.Volumes = []corev1.Volume{
+					{
+						Name: "configMapVolume",
+						VolumeSource: corev1.VolumeSource{
+							ConfigMap: &corev1.ConfigMapVolumeSource{},
+						},
+					},
+					{
+						Name: "emptyDirVolume",
+						VolumeSource: corev1.VolumeSource{
+							EmptyDir: &corev1.EmptyDirVolumeSource{},
+						},
+					},
+					{
+						Name: "projectedVolume",
+						VolumeSource: corev1.VolumeSource{
+							Projected: &corev1.ProjectedVolumeSource{},
+						},
+					},
+				}
+				Expect(fakeClient.Create(ctx, podWithPermittedVolumes)).To(Succeed())
+			},
+			nil,
+			[]rule.CheckResult{
+				{Status: rule.Passed, Message: "Pod uses only allowed volume types.", Target: rule.NewTarget("kind", "pod", "name", "podWithPermittedVolumes", "namespace", "plainNamespace")},
+			},
+		),
+		Entry("should fail when a pod volume is not of an allowed type",
+			func() {
+				podWithPermittedVolumes := plainPod.DeepCopy()
+				podWithPermittedVolumes.Name = "podWithPermittedVolumes"
+				podWithPermittedVolumes.Spec.Volumes = []corev1.Volume{
+					{
+						Name: "emptyDirVolume",
+						VolumeSource: corev1.VolumeSource{
+							EmptyDir: &corev1.EmptyDirVolumeSource{},
+						},
+					},
+					{
+						Name: "cinderVolume",
+						VolumeSource: corev1.VolumeSource{
+							Cinder: &corev1.CinderVolumeSource{},
+						},
+					},
+				}
+				Expect(fakeClient.Create(ctx, podWithPermittedVolumes)).To(Succeed())
+			},
+			nil,
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "Pod uses not allowed volume type.", Target: rule.NewTarget("kind", "pod", "name", "podWithPermittedVolumes", "namespace", "plainNamespace", "volume", "cinderVolume")},
+			},
+		),
+		Entry("should accept a volume when it is specified in the options configuration",
+			func() {
+
+				acceptedPod := plainPod.DeepCopy()
+				acceptedPod.Name = "acceptedPod"
+				acceptedPod.Labels = map[string]string{"podFoo": "podBar"}
+				acceptedPod.Namespace = namespaceName
+				acceptedPod.Spec.Volumes = []corev1.Volume{
+					{
+						Name: "acceptedVolume1",
+						VolumeSource: corev1.VolumeSource{
+							Cinder: &corev1.CinderVolumeSource{},
+						},
+					},
+					{
+						Name: "permittedVolume1",
+						VolumeSource: corev1.VolumeSource{
+							ConfigMap: &corev1.ConfigMapVolumeSource{},
+						},
+					},
+				}
+				Expect(fakeClient.Create(ctx, acceptedPod)).To(Succeed())
+			},
+			&rules.Options2003{
+				AcceptedPods: []rules.AcceptedPods2003{
+					{
+						AcceptedNamespacedObject: option.AcceptedNamespacedObject{
+							NamespacedObjectSelector: option.NamespacedObjectSelector{
+								NamespaceMatchLabels: map[string]string{
+									"foo": "bar",
+								},
+								MatchLabels: map[string]string{
+									"podFoo": "podBar",
+								},
+							},
+							Justification: "justification 1",
+						},
+						VolumeNames: []string{"acceptedVolume1"},
+					},
+					{
+						AcceptedNamespacedObject: option.AcceptedNamespacedObject{},
+						VolumeNames:              []string{"acceptedVolume1"},
+					},
+				},
+			},
+			[]rule.CheckResult{
+				{Status: rule.Accepted, Message: "justification 1", Target: rule.NewTarget("kind", "pod", "name", "acceptedPod", "namespace", "plainNamespace", "volume", "acceptedVolume1")},
+			},
+		),
+		Entry("should return appropriate check results for multiple pods",
+			func() {
+
+				acceptedPod := plainPod.DeepCopy()
+				acceptedPod.Name = "acceptedPod"
+				acceptedPod.Labels = map[string]string{"podFoo": "podBar"}
+				acceptedPod.Namespace = namespaceName
+				acceptedPod.Spec.Volumes = []corev1.Volume{
+					{
+						Name: "acceptedVolume1",
+						VolumeSource: corev1.VolumeSource{
+							Cinder: &corev1.CinderVolumeSource{},
+						},
+					},
+					{
+						Name: "permittedVolume1",
+						VolumeSource: corev1.VolumeSource{
+							Projected: &corev1.ProjectedVolumeSource{},
+						},
+					},
+					{
+						Name: "permittedVolume2",
+						VolumeSource: corev1.VolumeSource{
+							Ephemeral: &corev1.EphemeralVolumeSource{},
+						},
+					},
+					{
+						Name: "forbiddenVolume1",
+						VolumeSource: corev1.VolumeSource{
+							CephFS: &corev1.CephFSVolumeSource{},
+						},
+					},
+				}
+
+				Expect(fakeClient.Create(ctx, acceptedPod)).To(Succeed())
+
+				regularPod := plainPod.DeepCopy()
+				regularPod.Name = "regularPod"
+				regularPod.Namespace = namespaceName
+				regularPod.Spec.Volumes = []corev1.Volume{
+					{
+						Name: "acceptedVolume1",
+						VolumeSource: corev1.VolumeSource{
+							Cinder: &corev1.CinderVolumeSource{},
+						},
+					},
+					{
+						Name: "permittedVolume1",
+						VolumeSource: corev1.VolumeSource{
+							ConfigMap: &corev1.ConfigMapVolumeSource{},
+						},
+					},
+					{
+						Name: "forbiddenVolume1",
+						VolumeSource: corev1.VolumeSource{
+							Cinder: &corev1.CinderVolumeSource{},
+						},
+					},
+				}
+
+				Expect(fakeClient.Create(ctx, regularPod)).To(Succeed())
+			},
+			&rules.Options2003{
+				AcceptedPods: []rules.AcceptedPods2003{
+					{
+						AcceptedNamespacedObject: option.AcceptedNamespacedObject{
+							NamespacedObjectSelector: option.NamespacedObjectSelector{
+								NamespaceMatchLabels: map[string]string{
+									"foo": "bar",
+								},
+								MatchLabels: map[string]string{
+									"podFoo": "podBar",
+								},
+							},
+							Justification: "justification 1",
+						},
+						VolumeNames: []string{"acceptedVolume1"},
+					},
+					{
+						AcceptedNamespacedObject: option.AcceptedNamespacedObject{},
+						VolumeNames:              []string{"acceptedVolume1"},
+					},
+				},
+			},
+			[]rule.CheckResult{
+				{Status: rule.Accepted, Message: "justification 1", Target: rule.NewTarget("kind", "pod", "name", "acceptedPod", "namespace", "plainNamespace", "volume", "acceptedVolume1")},
+				{Status: rule.Failed, Message: "Pod uses not allowed volume type.", Target: rule.NewTarget("kind", "pod", "name", "acceptedPod", "namespace", "plainNamespace", "volume", "forbiddenVolume1")},
+				{Status: rule.Failed, Message: "Pod uses not allowed volume type.", Target: rule.NewTarget("kind", "pod", "name", "regularPod", "namespace", "plainNamespace", "volume", "acceptedVolume1")},
+				{Status: rule.Failed, Message: "Pod uses not allowed volume type.", Target: rule.NewTarget("kind", "pod", "name", "regularPod", "namespace", "plainNamespace", "volume", "forbiddenVolume1")},
+			},
+		),
+
+		Entry("should return appropriate check results for pods in multiple namespaces",
+			func() {
+				labeledNamespace := &corev1.Namespace{}
+				labeledNamespace.Name = "labeledNamespace"
+				labeledNamespace.Labels = map[string]string{
+					"label": "baz",
+				}
+				Expect(fakeClient.Create(ctx, labeledNamespace)).To(Succeed())
+
+				labeledNamespacePod := plainPod.DeepCopy()
+				labeledNamespacePod.Name = "labeledNamespacePod"
+				labeledNamespacePod.Labels = map[string]string{"podFoo": "podBar"}
+				labeledNamespacePod.Namespace = labeledNamespace.Name
+				labeledNamespacePod.Spec.Volumes = []corev1.Volume{
+					{
+						Name: "acceptedVolume1",
+						VolumeSource: corev1.VolumeSource{
+							Cinder: &corev1.CinderVolumeSource{},
+						},
+					},
+				}
+				Expect(fakeClient.Create(ctx, labeledNamespacePod)).To(Succeed())
+
+				plainNamespacePod := plainPod.DeepCopy()
+				plainNamespacePod.Name = "plainNamespacePod"
+				plainNamespacePod.Labels = map[string]string{"podFoo": "podBar"}
+				plainNamespacePod.Namespace = namespaceName
+				plainNamespacePod.Spec.Volumes = []corev1.Volume{
+					{
+						Name: "forbiddenVolume1",
+						VolumeSource: corev1.VolumeSource{
+							Cinder: &corev1.CinderVolumeSource{},
+						},
+					},
+				}
+				Expect(fakeClient.Create(ctx, plainNamespacePod)).To(Succeed())
+			},
+			&rules.Options2003{
+				AcceptedPods: []rules.AcceptedPods2003{
+					{
+						AcceptedNamespacedObject: option.AcceptedNamespacedObject{
+							NamespacedObjectSelector: option.NamespacedObjectSelector{
+								NamespaceMatchLabels: map[string]string{
+									"label": "baz",
+								},
+								MatchLabels: map[string]string{
+									"podFoo": "podBar",
+								},
+							},
+							Justification: "justification 1",
+						},
+						VolumeNames: []string{"acceptedVolume1"},
+					},
+					{
+						AcceptedNamespacedObject: option.AcceptedNamespacedObject{},
+						VolumeNames:              []string{"acceptedVolume1"},
+					},
+				},
+			},
+			[]rule.CheckResult{
+				{Status: rule.Accepted, Message: "justification 1", Target: rule.NewTarget("kind", "pod", "name", "labeledNamespacePod", "namespace", "labeledNamespace", "volume", "acceptedVolume1")},
+				{Status: rule.Failed, Message: "Pod uses not allowed volume type.", Target: rule.NewTarget("kind", "pod", "name", "plainNamespacePod", "namespace", "plainNamespace", "volume", "forbiddenVolume1")},
+			},
+		),
+	)
+
+	Describe("#ValidateOptions2003", func() {
+		It("should correctly validate options", func() {
+			options := rules.Options2003{
+				AcceptedPods: []rules.AcceptedPods2003{
+					{
+						AcceptedNamespacedObject: option.AcceptedNamespacedObject{
+							NamespacedObjectSelector: option.NamespacedObjectSelector{
+								MatchLabels: map[string]string{
+									"foo": "bar",
+								},
+								NamespaceMatchLabels: map[string]string{
+									"foo": "bar",
+								},
+							},
+						},
+					},
+					{
+						AcceptedNamespacedObject: option.AcceptedNamespacedObject{
+							NamespacedObjectSelector: option.NamespacedObjectSelector{
+								MatchLabels: map[string]string{
+									"foo": "bar",
+								},
+								NamespaceMatchLabels: map[string]string{
+									"foo": "bar",
+								},
+							},
+						},
+						VolumeNames: []string{"foo"},
+					},
+					{
+						AcceptedNamespacedObject: option.AcceptedNamespacedObject{
+							NamespacedObjectSelector: option.NamespacedObjectSelector{
+								MatchLabels: map[string]string{
+									"foo": "bar",
+								},
+								NamespaceMatchLabels: map[string]string{
+									"foo": "bar",
+								},
+							},
+						},
+						VolumeNames: []string{""},
+					},
+				},
+			}
+
+			result := options.Validate()
+
+			Expect(result).To(ConsistOf(
+				PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":   Equal(field.ErrorTypeRequired),
+					"Field":  Equal("acceptedPods.volumeNames"),
+					"Detail": Equal("must not be empty"),
+				})),
+				PointTo(MatchFields(IgnoreExtras, Fields{
+					"Type":     Equal(field.ErrorTypeInvalid),
+					"Field":    Equal("acceptedPods.volumeNames[0]"),
+					"BadValue": Equal(""),
+					"Detail":   Equal("must not be empty"),
+				})),
+			))
+		})
+	})
+})
