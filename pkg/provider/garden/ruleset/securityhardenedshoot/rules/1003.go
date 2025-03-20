82d117205c24d70e5a0521920233a711d2584db2
Aleksandar Savchev <aleksandar.savchev@sap.com>
2025-03-20 16:04:04 +0200

    Add rule 1003 implementation

---
+++ pkg/provider/garden/ruleset/securityhardenedshoot/rules/1003.go
@@ -0,0 +1,96 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package rules
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	lakomapiv1alpha1 "github.com/gardener/gardener-extension-shoot-lakom-service/pkg/apis/lakom/v1alpha1"
+	lakomconst "github.com/gardener/gardener-extension-shoot-lakom-service/pkg/constants"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/kubectl/pkg/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/gardener/diki/pkg/rule"
+)
+
+var (
+	_ rule.Rule     = &Rule1003{}
+	_ rule.Severity = &Rule1003{}
+)
+
+type Rule1003 struct {
+	Client         client.Client
+	ShootName      string
+	ShootNamespace string
+}
+
+func (r *Rule1003) ID() string {
+	return "1003"
+}
+
+func (r *Rule1003) Name() string {
+	return "Shoot clusters should have Lakom extension configured correctly."
+}
+
+func (r *Rule1003) Severity() rule.SeverityLevel {
+	return rule.SeverityMedium
+}
+
+func (r *Rule1003) Run(ctx context.Context) (rule.RuleResult, error) {
+	shoot := &gardencorev1beta1.Shoot{ObjectMeta: v1.ObjectMeta{Name: r.ShootName, Namespace: r.ShootNamespace}}
+	if err := r.Client.Get(ctx, client.ObjectKeyFromObject(shoot), shoot); err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("name", r.ShootName, "namespace", r.ShootNamespace, "kind", "Shoot"))), nil
+	}
+
+	var (
+		lakomExtensionType = lakomconst.ExtensionType
+		extensionIndex     = slices.IndexFunc(shoot.Spec.Extensions, func(shootSpecExtension gardencorev1beta1.Extension) bool {
+			return shootSpecExtension.Type == lakomExtensionType
+		})
+		extensionDisabled                         = extensionIndex >= 0 && shoot.Spec.Extensions[extensionIndex].Disabled != nil && *shoot.Spec.Extensions[extensionIndex].Disabled
+		extensionLabelValue, extensionLabelExists = shoot.Labels["extensions.extensions.gardener.cloud/"+lakomExtensionType]
+	)
+
+	switch {
+	case !extensionLabelExists:
+		return rule.Result(r, rule.FailedCheckResult(fmt.Sprintf("Extension %s is not configured for the shoot cluster.", lakomExtensionType), rule.NewTarget())), nil
+	case extensionLabelValue == "true" && !extensionDisabled && extensionIndex >= 0:
+		var (
+			lakomExtension = shoot.Spec.Extensions[extensionIndex]
+			lakomConfig    = &lakomapiv1alpha1.LakomConfig{}
+			scheme         = scheme.Scheme
+		)
+
+		if lakomExtension.ProviderConfig == nil {
+			return rule.Result(r, rule.FailedCheckResult(fmt.Sprintf("Extension %s does not have provider config configured for the shoot cluster.", lakomExtensionType), rule.NewTarget())), nil
+		}
+
+		if err := lakomapiv1alpha1.AddToScheme(scheme); err != nil {
+			return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget())), nil
+		}
+
+		_, _, err := serializer.NewCodecFactory(scheme).UniversalDeserializer().Decode(lakomExtension.ProviderConfig.Raw, nil, lakomConfig)
+		if err != nil {
+			return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget())), nil
+		}
+
+		if lakomConfig.TrustedKeysResourceName == nil || len(*lakomConfig.TrustedKeysResourceName) == 0 {
+			return rule.Result(r, rule.FailedCheckResult(fmt.Sprintf("Extension %s does not have TrustedKeysResourceName configured.", lakomExtensionType), rule.NewTarget())), nil
+		} else {
+			return rule.Result(r, rule.PassedCheckResult(fmt.Sprintf("Extension %s is correctly configured for the shoot cluster.", lakomExtensionType), rule.NewTarget())), nil
+		}
+	case extensionLabelValue == "true" && !extensionDisabled:
+		return rule.Result(r, rule.FailedCheckResult(fmt.Sprintf("Extension %s does not have extension config configured for the shoot cluster.", lakomExtensionType), rule.NewTarget())), nil
+	case extensionLabelValue == "true" && extensionDisabled:
+		return rule.Result(r, rule.FailedCheckResult(fmt.Sprintf("Extension %s is disabled in the shoot spec and enabled in labels.", lakomExtensionType), rule.NewTarget())), nil
+	default:
+		return rule.Result(r, rule.FailedCheckResult(fmt.Sprintf("Extension %s has unexpected label value: %s.", lakomExtensionType, extensionLabelValue), rule.NewTarget())), nil
+	}
+}
---
+++ pkg/provider/garden/ruleset/securityhardenedshoot/rules/1003_test.go
@@ -0,0 +1,210 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package rules_test
+
+import (
+	"context"
+	"encoding/json"
+
+	lakomv1alpha1 "github.com/gardener/gardener-extension-shoot-lakom-service/pkg/apis/lakom/v1alpha1"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	"github.com/gardener/gardener/pkg/client/kubernetes"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/gardener/diki/pkg/provider/garden/ruleset/securityhardenedshoot/rules"
+	"github.com/gardener/diki/pkg/rule"
+)
+
+var _ = Describe("#1003", func() {
+	var (
+		fakeClient client.Client
+		ctx        = context.TODO()
+
+		shoot *gardencorev1beta1.Shoot
+
+		shootName      = "foo"
+		shootNamespace = "bar"
+
+		r        rule.Rule
+		ruleID   = "1003"
+		ruleName = "Shoot clusters should have Lakom extension configured correctly."
+		severity = rule.SeverityMedium
+
+		encode = func(obj runtime.Object) []byte {
+			data, err := json.Marshal(obj)
+			Expect(err).ToNot(HaveOccurred())
+
+			return data
+		}
+	)
+
+	BeforeEach(func() {
+		fakeClient = fakeclient.NewClientBuilder().WithScheme(kubernetes.GardenScheme).Build()
+		shoot = &gardencorev1beta1.Shoot{ObjectMeta: metav1.ObjectMeta{Name: shootName, Namespace: shootNamespace}}
+	})
+
+	DescribeTable("Run cases", func(updateFn func(), expectedCheckResults []rule.CheckResult) {
+		updateFn()
+
+		Expect(fakeClient.Create(ctx, shoot)).To(Succeed())
+
+		r = &rules.Rule1003{
+			Client:         fakeClient,
+			ShootName:      shootName,
+			ShootNamespace: shootNamespace,
+		}
+
+		res, err := r.Run(ctx)
+		Expect(err).To(BeNil())
+		Expect(res).To(Equal(rule.RuleResult{RuleID: ruleID, RuleName: ruleName, Severity: severity, CheckResults: expectedCheckResults}))
+	},
+		Entry("should error when the shoot can't be found",
+			func() { shoot.Name = "notFoo" },
+			[]rule.CheckResult{
+				{Status: rule.Errored, Message: "shoots.core.gardener.cloud \"foo\" not found", Target: rule.NewTarget("name", "foo", "namespace", "bar", "kind", "Shoot")},
+			},
+		),
+		Entry("should fail when Lakom extension is not configured",
+			func() {
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type: "shoot-lakom-service",
+					},
+				}
+			},
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "Extension shoot-lakom-service is not configured for the shoot cluster.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should fail when Lakom extension does not have extension configuration",
+			func() {
+				shoot.Labels = map[string]string{
+					"extensions.extensions.gardener.cloud/shoot-lakom-service": "true",
+				}
+			},
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "Extension shoot-lakom-service does not have extension config configured for the shoot cluster.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should fail when Lakom extension is disabled",
+			func() {
+				shoot.Labels = map[string]string{
+					"extensions.extensions.gardener.cloud/shoot-lakom-service": "true",
+				}
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type:     "shoot-lakom-service",
+						Disabled: ptr.To(true),
+					},
+				}
+			},
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "Extension shoot-lakom-service is disabled in the shoot spec and enabled in labels.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should fail when Lakom extension has unecpected value in the shoot labels",
+			func() {
+				shoot.Labels = map[string]string{
+					"extensions.extensions.gardener.cloud/shoot-lakom-service": "false",
+				}
+			},
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "Extension shoot-lakom-service has unexpected label value: false.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should fail when Lakom extension does not have provider config",
+			func() {
+				shoot.Labels = map[string]string{
+					"extensions.extensions.gardener.cloud/shoot-lakom-service": "true",
+				}
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type: "shoot-lakom-service",
+					},
+				}
+			},
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "Extension shoot-lakom-service does not have provider config configured for the shoot cluster.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should fail when Lakom extension does not have trustedKeysResourceName set",
+			func() {
+				shoot.Labels = map[string]string{
+					"extensions.extensions.gardener.cloud/shoot-lakom-service": "true",
+				}
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type: "shoot-lakom-service",
+						ProviderConfig: &runtime.RawExtension{
+							Raw: encode(&lakomv1alpha1.LakomConfig{
+								TypeMeta: metav1.TypeMeta{
+									APIVersion: lakomv1alpha1.SchemeGroupVersion.String(),
+									Kind:       "LakomConfig",
+								},
+							}),
+						},
+					},
+				}
+			},
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "Extension shoot-lakom-service does not have TrustedKeysResourceName configured.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should fail when Lakom extension has trustedKeysResourceName set to empty string",
+			func() {
+				shoot.Labels = map[string]string{
+					"extensions.extensions.gardener.cloud/shoot-lakom-service": "true",
+				}
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type: "shoot-lakom-service",
+						ProviderConfig: &runtime.RawExtension{
+							Raw: encode(&lakomv1alpha1.LakomConfig{
+								TypeMeta: metav1.TypeMeta{
+									APIVersion: lakomv1alpha1.SchemeGroupVersion.String(),
+									Kind:       "LakomConfig",
+								},
+								TrustedKeysResourceName: ptr.To(""),
+							}),
+						},
+					},
+				}
+			},
+			[]rule.CheckResult{
+				{Status: rule.Failed, Message: "Extension shoot-lakom-service does not have TrustedKeysResourceName configured.", Target: rule.NewTarget()},
+			},
+		),
+		Entry("should pass when Lakom extension has trustedKeysResourceName set",
+			func() {
+				shoot.Labels = map[string]string{
+					"extensions.extensions.gardener.cloud/shoot-lakom-service": "true",
+				}
+				shoot.Spec.Extensions = []gardencorev1beta1.Extension{
+					{
+						Type: "shoot-lakom-service",
+						ProviderConfig: &runtime.RawExtension{
+							Raw: encode(&lakomv1alpha1.LakomConfig{
+								TypeMeta: metav1.TypeMeta{
+									APIVersion: lakomv1alpha1.SchemeGroupVersion.String(),
+									Kind:       "LakomConfig",
+								},
+								TrustedKeysResourceName: ptr.To("foo"),
+							}),
+						},
+					},
+				}
+			},
+			[]rule.CheckResult{
+				{Status: rule.Passed, Message: "Extension shoot-lakom-service is correctly configured for the shoot cluster.", Target: rule.NewTarget()},
+			},
+		),
+	)
+})
