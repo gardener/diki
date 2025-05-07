76918ec8443a77437ad671ee6105670e5c22892f
georgibaltiev <georgi.baltiev@sap.com>
2025-05-07 10:48:20 +0300

    Reconfigure and add unit tests to validate the new checks

--- pkg/provider/managedk8s/ruleset/securityhardenedk8s/rules/2000_test.go
+++ pkg/provider/managedk8s/ruleset/securityhardenedk8s/rules/2000_test.go
@@ -59,6 +59,19 @@ var _ = Describe("#2000", func() {
 						Ingress: true,
 					},
 				},
+				{
+					AcceptedClusterObject: option.AcceptedClusterObject{
+						ClusterObjectSelector: option.ClusterObjectSelector{
+							MatchLabels: map[string]string{
+								"role": "test1",
+							},
+						},
+						Justification: "justification test",
+					},
+					AcceptedTraffic: rules.AcceptedTraffic{
+						Egress: true,
+					},
+				},
 			},
 		}
 
@@ -170,6 +183,76 @@ var _ = Describe("#2000", func() {
 
 		Expect(client.Create(ctx, nsFoo)).To(Succeed())
 
+		nsTest1 := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test1",
+				Labels: map[string]string{
+					"role": "test1",
+				},
+			},
+		}
+
+		Expect(client.Create(ctx, nsTest1)).To(Succeed())
+
+		npTest1 := &networkingv1.NetworkPolicy{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "default-allow",
+				Namespace: "test1",
+			},
+			Spec: networkingv1.NetworkPolicySpec{
+				PolicyTypes: []networkingv1.PolicyType{
+					networkingv1.PolicyTypeIngress,
+					networkingv1.PolicyTypeEgress,
+				},
+				Ingress: []networkingv1.NetworkPolicyIngressRule{
+					{
+						Ports: []networkingv1.NetworkPolicyPort{},
+						From:  []networkingv1.NetworkPolicyPeer{},
+					},
+				},
+				Egress: []networkingv1.NetworkPolicyEgressRule{
+					{
+						Ports: []networkingv1.NetworkPolicyPort{},
+						To:    []networkingv1.NetworkPolicyPeer{},
+					},
+				},
+			},
+		}
+
+		Expect(client.Create(ctx, npTest1)).To(Succeed())
+
+		nsTest2 := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test2",
+				Labels: map[string]string{
+					"role": "test2",
+				},
+			},
+		}
+
+		Expect(client.Create(ctx, nsTest2)).To(Succeed())
+
+		npTest2 := &networkingv1.NetworkPolicy{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "mixed-allow",
+				Namespace: "test2",
+			},
+			Spec: networkingv1.NetworkPolicySpec{
+				PolicyTypes: []networkingv1.PolicyType{
+					networkingv1.PolicyTypeIngress,
+					networkingv1.PolicyTypeEgress,
+				},
+				Egress: []networkingv1.NetworkPolicyEgressRule{
+					{
+						Ports: []networkingv1.NetworkPolicyPort{},
+						To:    []networkingv1.NetworkPolicyPeer{},
+					},
+				},
+			},
+		}
+
+		Expect(client.Create(ctx, npTest2)).To(Succeed())
+
 		ruleResult, err := r.Run(ctx)
 		Expect(err).ToNot(HaveOccurred())
 
@@ -191,8 +274,8 @@ var _ = Describe("#2000", func() {
 			},
 			{
 				Status:  rule.Failed,
-				Message: "Egress traffic is not denied by default.",
-				Target:  rule.NewTarget("namespace", "kube-system"),
+				Message: "All Egress traffic is allowed by default.",
+				Target:  rule.NewTarget("namespace", "kube-system", "kind", "networkPolicy", "name", "deny-ingress"),
 			},
 			{
 				Status:  rule.Passed,
@@ -223,6 +306,26 @@ var _ = Describe("#2000", func() {
 				Status:  rule.Accepted,
 				Message: "justification foo",
 				Target:  rule.NewTarget("namespace", "kube-foo", "details", "traffic: ingress"),
+			},
+			{
+				Status:  rule.Failed,
+				Message: "All Ingress traffic is allowed by default.",
+				Target:  rule.NewTarget("namespace", "test1", "kind", "networkPolicy", "name", "default-allow"),
+			},
+			{
+				Status:  rule.Accepted,
+				Message: "justification test",
+				Target:  rule.NewTarget("namespace", "test1", "details", "traffic: egress"),
+			},
+			{
+				Status:  rule.Passed,
+				Message: "Ingress traffic is denied by default.",
+				Target:  rule.NewTarget("namespace", "test2", "kind", "networkPolicy", "name", "mixed-allow"),
+			},
+			{
+				Status:  rule.Failed,
+				Message: "All Egress traffic is allowed by default.",
+				Target:  rule.NewTarget("namespace", "test2", "kind", "networkPolicy", "name", "mixed-allow"),
 			},
 		}
 
