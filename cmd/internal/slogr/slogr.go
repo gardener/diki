8333b025933a6c7f39111bff85b22a5d10e85dc7
Aleksandar Savchev <aleksandar.savchev@sap.com>
2025-05-21 12:37:41 +0300

    Add suggested changes

--- cmd/internal/slogr/slogr.go
+++ cmd/internal/slogr/slogr.go
@@ -5,6 +5,7 @@
 package slogr
 
 import (
+	"context"
 	"log/slog"
 
 	"github.com/go-logr/logr"
@@ -14,24 +15,24 @@ var (
 	_ logr.LogSink = &Slogr{}
 )
 
-// Slogr is a wrapper around slog.Logger to implement the logr.LogSink interface.
+// Slogr is a wrapper around slog.Logger to implement the [logr.LogSink] interface.
 type Slogr struct {
 	logger *slog.Logger
 }
 
-// NewLogr creates a new logr.Logger from a slog.Logger.
+// NewLogr creates a new [logr.Logger] from a slog.Logger.
 func NewLogr(logger *slog.Logger) logr.Logger {
 	return logr.New(Slogr{logger: logger})
 }
 
-// Enabled implmenents the logr.LogSink interface.
-func (s Slogr) Enabled(_ int) bool {
-	return true
+// Enabled implmenents the [logr.LogSink] interface.
+func (s Slogr) Enabled(level int) bool {
+	return s.logger.Enabled(context.Background(), slog.Level(level))
 }
 
 // Error logs an error message.
 func (s Slogr) Error(err error, msg string, keysAndValues ...interface{}) {
-	s.logger.Error(msg, append(keysAndValues, "error", err)...)
+	s.logger.Error(msg, append([]interface{}{"error", err}, keysAndValues)...)
 }
 
 // Info logs an info message.
@@ -39,17 +40,17 @@ func (s Slogr) Info(_ int, msg string, keysAndValues ...interface{}) {
 	s.logger.Info(msg, keysAndValues...)
 }
 
-// Init implments the logr.LogSink interface.
+// Init implments the [logr.LogSink] interface.
 func (s Slogr) Init(_ logr.RuntimeInfo) {
 }
 
-// WithName returns a new logr.Logger with the specified  group name.
+// WithName returns a new [logr.Logger] with the specified  group name.
 func (s Slogr) WithName(name string) logr.LogSink {
 	s.logger.WithGroup(name)
 	return &s
 }
 
-// WithValues returns a new logr.Logger with the specified key-value pairs.
+// WithValues returns a new [logr.Logger] with the specified key-value pairs.
 func (s Slogr) WithValues(keysAndValues ...interface{}) logr.LogSink {
 	s.logger = s.logger.With(keysAndValues...)
 	return &s
