92daa9b19686e96143d4b2046d09ca0a232254a1
georgibaltiev <georgi.baltiev@sap.com>
2025-06-02 16:09:04 +0300

    Modify Rule 2000 of the security-hardened-shoot ruleset to check for anonymous authentication

--- pkg/provider/garden/ruleset/securityhardenedshoot/rules/2000.go
+++ pkg/provider/garden/ruleset/securityhardenedshoot/rules/2000.go
@@ -6,10 +6,14 @@ package rules
 
 import (
 	"context"
+	"fmt"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apiserverv1beta1 "k8s.io/apiserver/pkg/apis/apiserver/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/kustomize/kyaml/yaml"
 
 	"github.com/gardener/diki/pkg/rule"
 )
@@ -18,6 +22,8 @@ var (
 	_ rule.Rule     = &Rule2000{}
 	_ rule.Severity = &Rule2000{}
 )
+
+const fileName = "config.yaml"
 
 type Rule2000 struct {
 	Client         client.Client
@@ -43,13 +49,40 @@ func (r *Rule2000) Run(ctx context.Context) (rule.RuleResult, error) {
 		return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("name", r.ShootName, "namespace", r.ShootNamespace, "kind", "Shoot"))), nil
 	}
 
-	switch {
-	// EnableAnonymousAuthentication is deprecated but still used in the Gardener API
-	case shoot.Spec.Kubernetes.KubeAPIServer == nil || shoot.Spec.Kubernetes.KubeAPIServer.EnableAnonymousAuthentication == nil: //nolint:staticcheck
-		return rule.Result(r, rule.PassedCheckResult("Anonymous authentication is not enabled.", rule.NewTarget())), nil
-	case *shoot.Spec.Kubernetes.KubeAPIServer.EnableAnonymousAuthentication: //nolint:staticcheck
-		return rule.Result(r, rule.FailedCheckResult("Anonymous authentication is enabled for the kube-apiserver.", rule.NewTarget())), nil
-	default:
+	if shoot.Spec.Kubernetes.KubeAPIServer == nil {
+		return rule.Result(r, rule.PassedCheckResult("Anonymous authentication is not enabled for the kube-apiserver.", rule.NewTarget())), nil
+	}
+
+	// TODO (georgibaltiev): remove any references to the EnableAnonymousAuthentication field after it's deprecation
+	if shoot.Spec.Kubernetes.KubeAPIServer.EnableAnonymousAuthentication != nil { //nolint:staticcheck
+		if *shoot.Spec.Kubernetes.KubeAPIServer.EnableAnonymousAuthentication { //nolint:staticcheck
+			return rule.Result(r, rule.FailedCheckResult("Anonymous authentication is enabled for the kube-apiserver.", rule.NewTarget())), nil
+		}
 		return rule.Result(r, rule.PassedCheckResult("Anonymous authentication is disabled for the kube-apiserver.", rule.NewTarget())), nil
 	}
+
+	if shoot.Spec.Kubernetes.KubeAPIServer.StructuredAuthentication == nil {
+		return rule.Result(r, rule.PassedCheckResult("Anonymous authentication is not enabled for the kube-apiserver.", rule.NewTarget())), nil
+	}
+
+	configMapName := shoot.Spec.Kubernetes.KubeAPIServer.StructuredAuthentication.ConfigMapName
+	configMap := &corev1.ConfigMap{ObjectMeta: v1.ObjectMeta{Name: configMapName, Namespace: r.ShootNamespace}}
+	if err := r.Client.Get(ctx, client.ObjectKeyFromObject(configMap), configMap); err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("name", configMapName, "namespace", r.ShootNamespace, "kind", "ConfigMap"))), nil
+	}
+
+	authConfigString, ok := configMap.Data[fileName]
+	if !ok {
+		return rule.Result(r, rule.WarningCheckResult(fmt.Sprintf("The %s key is not present in the configMap.", fileName), rule.NewTarget("name", configMapName, "namespace", r.ShootNamespace, "kind", "ConfigMap"))), nil
+	}
+
+	authenticationConfig := &apiserverv1beta1.AuthenticationConfiguration{}
+	if err := yaml.Unmarshal([]byte(authConfigString), authenticationConfig); err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("name", configMapName, "namespace", r.ShootNamespace, "kind", "ConfigMap"))), nil
+	}
+
+	if authenticationConfig.Anonymous.Enabled {
+		return rule.Result(r, rule.FailedCheckResult("Anonymous authentication is enabled for the kube-apiserver.", rule.NewTarget("name", configMapName, "namespace", r.ShootNamespace, "kind", "ConfigMap"))), nil
+	}
+	return rule.Result(r, rule.PassedCheckResult("Anonymous authentication is disabled for the kube-apiserver.", rule.NewTarget("name", configMapName, "namespace", r.ShootNamespace, "kind", "ConfigMap"))), nil
 }
--- pkg/provider/garden/ruleset/securityhardenedshoot/rules/2000_test.go
+++ pkg/provider/garden/ruleset/securityhardenedshoot/rules/2000_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gardener/gardener/pkg/client/kubernetes"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -27,11 +28,22 @@ var _ = Describe("#2000", func() {
 		shootName      = "foo"
 		shootNamespace = "bar"
 
+		authenticationConfigMap *corev1.ConfigMap
+
 		shoot    *gardencorev1beta1.Shoot
 		r        rule.Rule
 		ruleName = "Shoot clusters must have anonymous authentication disabled for the Kubernetes API server."
 		ruleID   = "2000"
 		severity = rule.SeverityHigh
+	)
+
+	const (
+		fileName                                              = "config.yaml"
+		configMapName                                         = "authentication-config"
+		invalidAnonymousAuthenticationConfig                  = "foo"
+		disabledAnonymousAuthenticationConfig                 = "apiVersion: apiserver.config.k8s.io/v1beta1\nkind: AuthenticationConfiguration\nanonymous:\n  enabled: false"
+		enabledAnonymousAuthenticationConfigWithConditions    = "apiVersion: apiserver.config.k8s.io/v1beta1\nkind: AuthenticationConfiguration\nanonymous:\n  enabled: true\n  conditions:\n  - path: /healthz\n  - path: /livez"
+		enabledAnonymousAuthenticationConfigWithoutConditions = "apiVersion: apiserver.config.k8s.io/v1beta1\nkind: AuthenticationConfiguration\nanonymous:\n  enabled: true"
 	)
 
 	BeforeEach(func() {
@@ -44,6 +56,13 @@ var _ = Describe("#2000", func() {
 			},
 		}
 
+		authenticationConfigMap = &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      configMapName,
+				Namespace: shootNamespace,
+			},
+		}
+
 		r = &rules.Rule2000{
 			Client:         fakeClient,
 			ShootName:      shootName,
@@ -51,6 +70,7 @@ var _ = Describe("#2000", func() {
 		}
 	})
 
+	// TODO (georgibaltiev): remove any references to the EnableAnonymousAuthentication field after it's deprecation
 	DescribeTable("Run cases", func(updateFn func(), expectedCheckResult rule.CheckResult) {
 		updateFn()
 		Expect(fakeClient.Create(ctx, shoot)).To(Succeed())
@@ -64,21 +84,9 @@ var _ = Describe("#2000", func() {
 		),
 		Entry("should pass when kube-apiserver configuration is not set",
 			func() {},
-			rule.CheckResult{Status: rule.Passed, Message: "Anonymous authentication is not enabled.", Target: rule.NewTarget()},
+			rule.CheckResult{Status: rule.Passed, Message: "Anonymous authentication is not enabled for the kube-apiserver.", Target: rule.NewTarget()},
 		),
-		Entry("should pass when anonymous authentication is not set",
-			func() {
-				shoot.Spec.Kubernetes = gardencorev1beta1.Kubernetes{
-					KubeAPIServer: &gardencorev1beta1.KubeAPIServerConfig{
-						KubernetesConfig: gardencorev1beta1.KubernetesConfig{
-							FeatureGates: map[string]bool{"foo": true},
-						},
-					},
-				}
-			},
-			rule.CheckResult{Status: rule.Passed, Message: "Anonymous authentication is not enabled.", Target: rule.NewTarget()},
-		),
-		Entry("should pass when anonymous authentication is disabled",
+		Entry("should pass when the enabledAnoymousAuthentication flag is set to false",
 			func() {
 				shoot.Spec.Kubernetes = gardencorev1beta1.Kubernetes{
 					KubeAPIServer: &gardencorev1beta1.KubeAPIServerConfig{
@@ -88,7 +96,7 @@ var _ = Describe("#2000", func() {
 			},
 			rule.CheckResult{Status: rule.Passed, Message: "Anonymous authentication is disabled for the kube-apiserver.", Target: rule.NewTarget()},
 		),
-		Entry("should fail when anonymous authentication is enabled",
+		Entry("should fail the enabledAnoymousAuthentication flag is set to true",
 			func() {
 				shoot.Spec.Kubernetes = gardencorev1beta1.Kubernetes{
 					KubeAPIServer: &gardencorev1beta1.KubeAPIServerConfig{
@@ -97,6 +105,120 @@ var _ = Describe("#2000", func() {
 				}
 			},
 			rule.CheckResult{Status: rule.Failed, Message: "Anonymous authentication is enabled for the kube-apiserver.", Target: rule.NewTarget()},
+		),
+		Entry("should pass when neither the enableAnonymousAuthentication nor the structuredAuthenticaton flags are set",
+			func() {
+				shoot.Spec.Kubernetes = gardencorev1beta1.Kubernetes{
+					KubeAPIServer: &gardencorev1beta1.KubeAPIServerConfig{
+						KubernetesConfig: gardencorev1beta1.KubernetesConfig{
+							FeatureGates: map[string]bool{"foo": true},
+						},
+					},
+				}
+			},
+			rule.CheckResult{Status: rule.Passed, Message: "Anonymous authentication is not enabled for the kube-apiserver.", Target: rule.NewTarget()},
+		),
+		Entry("should error if the structuredAuthentication configMap is not present",
+			func() {
+				shoot.Spec.Kubernetes = gardencorev1beta1.Kubernetes{
+					KubeAPIServer: &gardencorev1beta1.KubeAPIServerConfig{
+						StructuredAuthentication: &gardencorev1beta1.StructuredAuthentication{
+							ConfigMapName: configMapName,
+						},
+					},
+				}
+			},
+			rule.CheckResult{Status: rule.Errored, Message: "configmaps \"authentication-config\" not found", Target: rule.NewTarget("name", configMapName, "namespace", shootNamespace, "kind", "ConfigMap")},
+		),
+		Entry("should warn if the structured authentication config does not contain a config.yaml key",
+			func() {
+				authenticationConfigMap.Data = map[string]string{
+					"foo": "bar",
+				}
+
+				Expect(fakeClient.Create(ctx, authenticationConfigMap)).To(Succeed())
+
+				shoot.Spec.Kubernetes = gardencorev1beta1.Kubernetes{
+					KubeAPIServer: &gardencorev1beta1.KubeAPIServerConfig{
+						StructuredAuthentication: &gardencorev1beta1.StructuredAuthentication{
+							ConfigMapName: configMapName,
+						},
+					},
+				}
+			},
+			rule.CheckResult{Status: rule.Warning, Message: "The config.yaml key is not present in the configMap.", Target: rule.NewTarget("name", configMapName, "namespace", shootNamespace, "kind", "ConfigMap")},
+		),
+		Entry("should error if the structuredAuthentication configMap contains an invalid value",
+			func() {
+				authenticationConfigMap.Data = map[string]string{
+					fileName: invalidAnonymousAuthenticationConfig,
+				}
+
+				Expect(fakeClient.Create(ctx, authenticationConfigMap)).To(Succeed())
+
+				shoot.Spec.Kubernetes = gardencorev1beta1.Kubernetes{
+					KubeAPIServer: &gardencorev1beta1.KubeAPIServerConfig{
+						StructuredAuthentication: &gardencorev1beta1.StructuredAuthentication{
+							ConfigMapName: configMapName,
+						},
+					},
+				}
+			},
+			rule.CheckResult{Status: rule.Errored, Message: "yaml: unmarshal errors:\n  line 1: cannot unmarshal !!str `foo` into v1beta1.AuthenticationConfiguration", Target: rule.NewTarget("name", configMapName, "namespace", shootNamespace, "kind", "ConfigMap")},
+		),
+		Entry("should fail if the structuredAuthentication configuration has anonymous access enabled unconditionally",
+			func() {
+				authenticationConfigMap.Data = map[string]string{
+					fileName: enabledAnonymousAuthenticationConfigWithoutConditions,
+				}
+
+				Expect(fakeClient.Create(ctx, authenticationConfigMap)).To(Succeed())
+
+				shoot.Spec.Kubernetes = gardencorev1beta1.Kubernetes{
+					KubeAPIServer: &gardencorev1beta1.KubeAPIServerConfig{
+						StructuredAuthentication: &gardencorev1beta1.StructuredAuthentication{
+							ConfigMapName: configMapName,
+						},
+					},
+				}
+			},
+			rule.CheckResult{Status: rule.Failed, Message: "Anonymous authentication is enabled for the kube-apiserver.", Target: rule.NewTarget("name", configMapName, "namespace", shootNamespace, "kind", "ConfigMap")},
+		),
+		Entry("should fail if the structured authentication config has anonymous access enabled with conditions",
+			func() {
+				authenticationConfigMap.Data = map[string]string{
+					fileName: enabledAnonymousAuthenticationConfigWithConditions,
+				}
+
+				Expect(fakeClient.Create(ctx, authenticationConfigMap)).To(Succeed())
+
+				shoot.Spec.Kubernetes = gardencorev1beta1.Kubernetes{
+					KubeAPIServer: &gardencorev1beta1.KubeAPIServerConfig{
+						StructuredAuthentication: &gardencorev1beta1.StructuredAuthentication{
+							ConfigMapName: configMapName,
+						},
+					},
+				}
+			},
+			rule.CheckResult{Status: rule.Failed, Message: "Anonymous authentication is enabled for the kube-apiserver.", Target: rule.NewTarget("name", configMapName, "namespace", shootNamespace, "kind", "ConfigMap")},
+		),
+		Entry("should pass if the structured authentication config has anonymous access disabled",
+			func() {
+				authenticationConfigMap.Data = map[string]string{
+					fileName: disabledAnonymousAuthenticationConfig,
+				}
+
+				Expect(fakeClient.Create(ctx, authenticationConfigMap)).To(Succeed())
+
+				shoot.Spec.Kubernetes = gardencorev1beta1.Kubernetes{
+					KubeAPIServer: &gardencorev1beta1.KubeAPIServerConfig{
+						StructuredAuthentication: &gardencorev1beta1.StructuredAuthentication{
+							ConfigMapName: configMapName,
+						},
+					},
+				}
+			},
+			rule.CheckResult{Status: rule.Passed, Message: "Anonymous authentication is disabled for the kube-apiserver.", Target: rule.NewTarget("name", configMapName, "namespace", shootNamespace, "kind", "ConfigMap")},
 		),
 	)
 })
