6938f1886b4941d1f12c5423486c86cf5a4dba16
georgibaltiev <georgi.baltiev@sap.com>
2025-06-25 15:28:38 +0300

    Refactor Targets for Rule 2001 (SHSC)

--- pkg/provider/garden/ruleset/securityhardenedshoot/rules/2001.go
+++ pkg/provider/garden/ruleset/securityhardenedshoot/rules/2001.go
@@ -9,8 +9,10 @@ import (
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
+	kubeutils "github.com/gardener/diki/pkg/kubernetes/utils"
 	"github.com/gardener/diki/pkg/rule"
 )
 
@@ -40,7 +42,7 @@ func (r *Rule2001) Severity() rule.SeverityLevel {
 func (r *Rule2001) Run(ctx context.Context) (rule.RuleResult, error) {
 	shoot := &gardencorev1beta1.Shoot{ObjectMeta: metav1.ObjectMeta{Name: r.ShootName, Namespace: r.ShootNamespace}}
 	if err := r.Client.Get(ctx, client.ObjectKeyFromObject(shoot), shoot); err != nil {
-		return rule.Result(r, rule.ErroredCheckResult(err.Error(), rule.NewTarget("name", r.ShootName, "namespace", r.ShootNamespace, "kind", "Shoot"))), nil
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), kubeutils.TargetWithK8sObject(rule.NewTarget(), v1.TypeMeta{Kind: "Shoot"}, shoot.ObjectMeta))), nil
 	}
 
 	switch {
