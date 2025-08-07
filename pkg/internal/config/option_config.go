441d6c53d6723d87832ba7d643a674b389a6b25b
georgibaltiev <georgi.baltiev@sap.com>
2025-08-07 08:55:12 +0300

    Fix typo

--- pkg/internal/config/option_config.go
+++ pkg/internal/config/option_config.go
@@ -9,6 +9,6 @@ import "github.com/gardener/diki/pkg/config"
 // IndexedRuleOptionsConfig represents per rule options and the index at which the option is configured in the ruleOptions configuration.
 type IndexedRuleOptionsConfig struct {
 	config.RuleOptionsConfig
-	// Index is the rule option's index in the ruleOptions configuration
+	// Index is the rule option's index in the ruleOptions configuration.
 	Index int
 }
