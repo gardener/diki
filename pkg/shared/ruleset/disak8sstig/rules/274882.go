213da25e6acc3672375c7f166ea20ebc186603cb
georgibaltiev <georgi.baltiev@sap.com>
2025-10-07 16:50:00 +0300

    Add rule 274882 of the DISA STIG ruleset

---
+++ pkg/shared/ruleset/disak8sstig/rules/274882.go
@@ -0,0 +1,116 @@
+// SPDX-FileCopyrightText: 2025 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package rules
+
+import (
+	"context"
+	"fmt"
+	"slices"
+
+	"github.com/goccy/go-yaml"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apiserverv1 "k8s.io/apiserver/pkg/apis/apiserver"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	kubeutils "github.com/gardener/diki/pkg/kubernetes/utils"
+	"github.com/gardener/diki/pkg/rule"
+)
+
+var (
+	_ rule.Rule     = &Rule274882{}
+	_ rule.Severity = &Rule274882{}
+)
+
+type Rule274882 struct {
+	Client         client.Client
+	Namespace      string
+	DeploymentName string
+	ContainerName  string
+}
+
+func (r *Rule274882) ID() string {
+	return ID274882
+}
+
+func (r *Rule274882) Name() string {
+	return "The Kubernetes API server must have anonymous authentication disabled."
+}
+
+func (r *Rule274882) Severity() rule.SeverityLevel {
+	return rule.SeverityHigh
+}
+
+func (r *Rule274882) Run(ctx context.Context) (rule.RuleResult, error) {
+
+	const encryptionProviderConfigOption = "encryption-provider-config"
+
+	deploymentName := "kube-apiserver"
+	containerName := "kube-apiserver"
+
+	if r.DeploymentName != "" {
+		deploymentName = r.DeploymentName
+	}
+
+	if r.ContainerName != "" {
+		containerName = r.ContainerName
+	}
+
+	target := rule.NewTarget("name", deploymentName, "namespace", r.Namespace, "kind", "Deployment")
+
+	optSlice, err := kubeutils.GetCommandOptionFromDeployment(ctx, r.Client, deploymentName, containerName, r.Namespace, encryptionProviderConfigOption)
+	if err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), target)), nil
+	}
+
+	if len(optSlice) > 1 {
+		return rule.Result(r, rule.WarningCheckResult(fmt.Sprintf("Option %s has been set more than once in the container command.", encryptionProviderConfigOption), target)), nil
+	} else if len(optSlice) == 0 {
+		return rule.Result(r, rule.FailedCheckResult(fmt.Sprintf("Option %s has not been set in the container command.", encryptionProviderConfigOption), target)), nil
+	}
+
+	kubeAPIServerDeployment := &appsv1.Deployment{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      deploymentName,
+			Namespace: r.Namespace,
+		},
+	}
+
+	if err := r.Client.Get(ctx, client.ObjectKeyFromObject(kubeAPIServerDeployment), kubeAPIServerDeployment); err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), target)), nil
+	}
+
+	bytes, err := kubeutils.GetVolumeConfigByteSliceByMountPath(ctx, r.Client, kubeAPIServerDeployment, containerName, optSlice[0])
+	if err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), target)), nil
+	}
+
+	encryptionProviderConfig := &apiserverv1.EncryptionConfiguration{}
+	if err := yaml.Unmarshal(bytes, encryptionProviderConfig); err != nil {
+		return rule.Result(r, rule.ErroredCheckResult(err.Error(), target)), nil
+	}
+
+	if len(encryptionProviderConfig.Resources) == 0 {
+		return rule.Result(r, rule.FailedCheckResult("Secrets are not explicitly encrypted at REST.", target)), nil
+	}
+
+	for _, resourceConfig := range encryptionProviderConfig.Resources {
+		if slices.ContainsFunc(resourceConfig.Resources, func(resourceName string) bool {
+			return resourceName == "*." || resourceName == "*.*" || resourceName == "secrets"
+		}) {
+			if len(resourceConfig.Providers) == 0 {
+				return rule.Result(r, rule.FailedCheckResult("Secrets are not explicitly encrypted at REST.", target)), nil
+			}
+
+			if resourceConfig.Providers[0].Identity != nil {
+				return rule.Result(r, rule.FailedCheckResult("Secrets are explicitly stored as plain text.", target)), nil
+			}
+
+			return rule.Result(r, rule.PassedCheckResult("Secrets are encrypted at REST.", target)), nil
+		}
+	}
+
+	return rule.Result(r, rule.FailedCheckResult("Secrets are not explicitly encrypted at REST.", target)), nil
+}
---
+++ pkg/shared/ruleset/disak8sstig/rules/274882_test.go
@@ -0,0 +1,275 @@
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package rules_test
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	gomegatypes "github.com/onsi/gomega/types"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/gardener/diki/pkg/rule"
+	"github.com/gardener/diki/pkg/shared/ruleset/disak8sstig/rules"
+)
+
+var _ = Describe("#274882", func() {
+
+	var (
+		fakeClient client.Client
+		ctx        = context.TODO()
+		namespace  = "foo"
+
+		ksDeployment *appsv1.Deployment
+		target       = rule.NewTarget("name", "kube-apiserver", "namespace", namespace, "kind", "Deployment")
+	)
+
+	BeforeEach(func() {
+		fakeClient = fakeclient.NewClientBuilder().Build()
+		ksDeployment = &appsv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "kube-apiserver",
+				Namespace: namespace,
+			},
+			Spec: appsv1.DeploymentSpec{
+				Template: corev1.PodTemplateSpec{
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:    "kube-apiserver",
+								Command: []string{},
+								Args:    []string{},
+							},
+						},
+					},
+				},
+			},
+		}
+	})
+
+	It("should error when kube-apiserver is not found", func() {
+		r := &rules.Rule274882{Client: fakeClient, Namespace: namespace}
+
+		ruleResult, err := r.Run(ctx)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(ruleResult.CheckResults).To(Equal([]rule.CheckResult{
+			{
+				Status:  rule.Errored,
+				Message: "deployments.apps \"kube-apiserver\" not found",
+				Target:  target,
+			},
+		}))
+	})
+
+	DescribeTable("Run cases for encryption-provider-config flag",
+		func(container corev1.Container, expectedCheckResults []rule.CheckResult, errorMatcher gomegatypes.GomegaMatcher) {
+			ksDeployment.Spec.Template.Spec.Containers = []corev1.Container{container}
+			Expect(fakeClient.Create(ctx, ksDeployment)).To(Succeed())
+
+			r := &rules.Rule274882{Client: fakeClient, Namespace: namespace}
+			ruleResult, err := r.Run(ctx)
+			Expect(err).To(errorMatcher)
+
+			Expect(ruleResult.CheckResults).To(Equal(expectedCheckResults))
+		},
+		Entry("should warn when encryption-provider-config is set more than once",
+			corev1.Container{Name: "kube-apiserver", Command: []string{"--encryption-provider-config=/etc/kubernetes/foo", "--encryption-provider-config=/etc/kubernetes/foo"}},
+			[]rule.CheckResult{{Status: rule.Warning, Message: "Option encryption-provider-config has been set more than once in the container command.", Target: target}},
+			BeNil()),
+		Entry("should error when deployment does not have container 'kube-apiserver'",
+			corev1.Container{Name: "not-kube-apiserver", Command: []string{"--encryption-provider-config=/etc/kubernetes/foo"}},
+			[]rule.CheckResult{{Status: rule.Errored, Message: "deployment: kube-apiserver does not contain container: kube-apiserver", Target: target}},
+			BeNil()),
+		Entry("should fail when encryption-provider-config is not set",
+			corev1.Container{Name: "kube-apiserver", Command: []string{}},
+			[]rule.CheckResult{{Status: rule.Failed, Message: "Option encryption-provider-config has not been set in the container command.", Target: target}},
+			BeNil()),
+	)
+
+	DescribeTable("Run cases for an EncryptionConfiguration object",
+		func(encryptionProviderConfig string, expectedCheckResult rule.CheckResult) {
+			ksDeployment.Spec.Template.Spec.Containers = []corev1.Container{
+				{
+					Name:    "kube-apiserver",
+					Command: []string{"./command"},
+					Args:    []string{"--encryption-provider-config=/etc/kubernetes/foo"},
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:      "encryption-provider-config",
+							MountPath: "/etc/kubernetes/foo",
+						},
+					},
+				},
+			}
+
+			ksDeployment.Spec.Template.Spec.Volumes = []corev1.Volume{
+				{
+					Name: "encryption-provider-config",
+					VolumeSource: corev1.VolumeSource{
+						ConfigMap: &corev1.ConfigMapVolumeSource{
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: "encryption-provider-config",
+							},
+						},
+					},
+				},
+			}
+			Expect(fakeClient.Create(ctx, ksDeployment)).To(Succeed())
+
+			configMap := &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "encryption-provider-config",
+					Namespace: namespace,
+				},
+				Data: map[string]string{
+					"/etc/kubernetes/foo": encryptionProviderConfig,
+				},
+			}
+			Expect(fakeClient.Create(ctx, configMap)).To(Succeed())
+
+			r := &rules.Rule274882{Client: fakeClient, Namespace: namespace}
+			ruleResult, err := r.Run(ctx)
+
+			Expect(err).To(BeNil())
+			Expect(ruleResult.CheckResults[0]).To(Equal(expectedCheckResult))
+		},
+		Entry("should error when the encryption config is not valid yaml",
+			"foo",
+			rule.CheckResult{Status: "Errored", Message: "[1:1] string was used where mapping is expected\n>  1 | foo\n       ^\n", Target: target},
+		),
+		Entry("should fail when the encryption config has no resources configured",
+			`
+apiVersion: apiserver.config.k8s.io/v1
+kind: EncryptionConfiguration`,
+			rule.CheckResult{Status: "Failed", Message: "Secrets are not explicitly encrypted at REST.", Target: target},
+		),
+		Entry("should fail when the encryption config does not include secrets as a resource for encryption",
+			`
+apiVersion: apiserver.config.k8s.io/v1
+kind: EncryptionConfiguration
+resources:
+- resources:
+  - pods
+  providers:
+  - aesgcm:
+    keys:
+    - name: key1
+      secret: c2VjcmV0IGlzIHNlY3VyZQ==`,
+			rule.CheckResult{Status: "Failed", Message: "Secrets are not explicitly encrypted at REST.", Target: target},
+		),
+		Entry("should fail when the encryption config includes secrets as a resource but encrypts then with identity",
+			`
+apiVersion: apiserver.config.k8s.io/v1
+kind: EncryptionConfiguration
+resources:
+- resources:
+  - secrets
+  providers:
+  - identity: {}`,
+			rule.CheckResult{Status: "Failed", Message: "Secrets are explicitly stored as plain text.", Target: target},
+		),
+		Entry("should fail when encryption config includes secrets as a resource and contains multiple providers but identity is the primary one",
+			`
+apiVersion: apiserver.config.k8s.io/v1
+kind: EncryptionConfiguration
+resources:
+- resources:
+  - secrets
+  providers:
+  - identity: {}
+  - aesgcm:
+    keys:
+    - name: key1
+      secret: c2VjcmV0IGlzIHNlY3VyZQ==`,
+			rule.CheckResult{Status: "Failed", Message: "Secrets are explicitly stored as plain text.", Target: target},
+		),
+		Entry("should pass when encryption config includes secrets as a resource and contains multiple providers and a valid one is the primary one",
+			`
+apiVersion: apiserver.config.k8s.io/v1
+kind: EncryptionConfiguration
+resources:
+- resources:
+  - secrets
+  providers:
+  - aesgcm:
+    keys:
+    - name: key1
+      secret: c2VjcmV0IGlzIHNlY3VyZQ==
+  - identity: {}`,
+			rule.CheckResult{Status: "Passed", Message: "Secrets are encrypted at REST.", Target: target}),
+		Entry("should fail when encryption config includes a wildcard resource for secrets but identity is the primary provider",
+			`
+apiVersion: apiserver.config.k8s.io/v1
+kind: EncryptionConfiguration
+resources:
+- resources:
+  - "*.*"
+  providers:
+  - identity: {}
+  - aesgcm:
+    keys:
+    - name: key1
+      secret: c2VjcmV0IGlzIHNlY3VyZQ==`,
+			rule.CheckResult{Status: "Failed", Message: "Secrets are explicitly stored as plain text.", Target: target}),
+		Entry("should pass when encryption configuration includes a wildcard resource for secrets and a valid primary provider",
+			`
+apiVersion: apiserver.config.k8s.io/v1
+kind: EncryptionConfiguration
+resources:
+- resources:
+  - "*."
+  providers:
+  - aesgcm:
+    keys:
+    - name: key1
+      secret: c2VjcmV0IGlzIHNlY3VyZQ==
+  - identity: {}`,
+			rule.CheckResult{Status: "Passed", Message: "Secrets are encrypted at REST.", Target: target},
+		),
+		Entry("should pass when there are multiple resource entries and secrets are properly encrypted in one of them",
+			`
+apiVersion: apiserver.config.k8s.io/v1
+kind: EncryptionConfiguration
+resources:
+- resources:
+  - secrets
+  providers:
+  - aesgcm:
+    keys:
+    - name: key1
+      secret: c2VjcmV0IGlzIHNlY3VyZQ==
+  - identity: {}
+- resources:
+  - "*"
+  providers:
+  - identity: {}`,
+			rule.CheckResult{Status: "Passed", Message: "Secrets are encrypted at REST.", Target: target},
+		),
+		Entry("should fail when there are multiple resource entries and the order of precedence overwrites the secret encryption",
+			`
+apiVersion: apiserver.config.k8s.io/v1
+kind: EncryptionConfiguration
+resources:
+- resources:
+  - "*."
+  providers:
+  - identity: {}
+- resources:
+  - secrets
+  providers:
+  - aesgcm:
+    keys:
+    - name: key1
+      secret: c2VjcmV0IGlzIHNlY3VyZQ==
+  - identity: {}`, rule.CheckResult{Status: "Failed", Message: "Secrets are explicitly stored as plain text.", Target: target},
+		),
+	)
+})
