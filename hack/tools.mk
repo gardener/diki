a7f572384dd324431a6c9c36af54f59469ac76f3
Aleksandar Savchev <aleksandar.savchev@sap.com>
2026-07-21 11:45:28 +0300

    Fix typos dir

--- hack/tools.mk
+++ hack/tools.mk
@@ -13,3 +13,8 @@ TAILWINDCSS_VERSION ?= v3.3.3
 $(TAILWINDCSS): $(call tool_version_file,$(TAILWINDCSS),$(TAILWINDCSS_VERSION))
 	curl -L -o $(TAILWINDCSS) https://github.com/tailwindlabs/tailwindcss/releases/download/$(TAILWINDCSS_VERSION)/tailwindcss-$(shell uname -s | tr '[:upper:]' '[:lower:]' | sed 's/darwin/macos/')-$(shell uname -m | sed 's/x86_64/x64/')
 	chmod +x $(TAILWINDCSS)
+
+# Override typos target from gardener's tools.mk which incorrectly
+# references $(GARDENER_HACK_DIR)/tools/ instead of $(GARDENER_TOOL_DIR)/.
+$(TYPOS): $(call tool_version_file,$(TYPOS),$(TYPOS_VERSION))
+	@TYPOS_VERSION=$(TYPOS_VERSION) bash $(GARDENER_TOOL_DIR)/install-typos.sh
